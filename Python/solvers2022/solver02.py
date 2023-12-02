e5a0adb44d726b558988cedde7793d9c1af49ce6
Andrew <5436387+fidwell@users.noreply.github.com>
2023-12-01 20:39:42 -0500

    2022 puzzle 2

---
+++ Python/solvers2022/solver02.py
@@ -0,0 +1,53 @@
+from utilities import datareader
+
+def solve(part: int, use_sample: bool):
+    lines = datareader.read_lines(2022, 2, part, use_sample)
+
+    score = 0
+    for line in lines:
+        score += evaluate_part1(line[0], line[2]) if part == 1 else evaluate_part2(line[0], line[2]) 
+
+    return str(score)
+
+def evaluate_part1(opp: chr, me: chr):
+    score_for_outcome = outcome_score(opp, me)
+    return score_for_shape(me) + score_for_outcome
+
+def evaluate_part2(opp: chr, result: chr):
+    score_for_outcome = 0 if result == 'X' else 3 if result == 'Y' else 6
+    me = shape_needed(opp, result)
+    return score_for_shape(me) + score_for_outcome
+
+def outcome_score(opp: chr, me: chr):
+    # Wins
+    if (opp == 'A' and me == 'Y') or (opp == 'B' and me == 'Z') or (opp == 'C' and me == 'X'):
+        return 6
+
+    # Losses
+    if (opp == 'B' and me == 'X') or (opp == 'C' and me == 'Y') or (opp == 'A' and me == 'Z'):
+        return 0
+
+    # Draws
+    return 3
+
+def shape_needed(opp: chr, result: chr):
+    if opp == 'A':
+        return chr(ord(result) - 1) if result != 'X' else 'Z'
+
+    if opp == 'B':
+        return result
+
+    if opp == 'C':
+        return chr(ord(result) + 1) if result != 'Z' else 'X'
+
+    return None
+
+def score_for_shape(shape):
+    return 1 if shape == 'X' else 2 if shape == 'Y' else 3
+
+# A Rock
+# B Paper
+# C Scissors
+# X Rock / Lose
+# Y Paper / Draw
+# Z Scissors / Win
--- Python/tests/test_solver2022.py
+++ Python/tests/test_solver2022.py
@@ -1,5 +1,5 @@
 import unittest
-from solvers2022 import solver01
+from solvers2022 import solver01, solver02
 
 class TestSolver2022(unittest.TestCase):
     def test_solver01_part1_sample(self):
@@ -13,3 +13,16 @@ class TestSolver2022(unittest.TestCase):
 
     def test_solver01_part2(self):
         self.assertEqual(solver01.solve(2, False), "211189")
+
+
+    def test_solver02_part1_sample(self):
+        self.assertEqual(solver02.solve(1, True), "15")
+
+    def test_solver02_part1(self):
+        self.assertEqual(solver02.solve(1, False), "11841")
+        
+    def test_solver02_part2_sample(self):
+        self.assertEqual(solver02.solve(2, True), "12")
+
+    def test_solver02_part2(self):
+        self.assertEqual(solver02.solve(2, False), "13022")
