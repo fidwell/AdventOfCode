ce354ba47650078e7f3df7093ea14c9cb864cb5f
Andrew <5436387+fidwell@users.noreply.github.com>
2023-12-03 11:33:22 -0500

    Puzzle 3

---
+++ CSharp/AdventOfCode.Core/PuzzleSolvers/2023/Puzzle03Solver.cs
@@ -0,0 +1,83 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCode.Data;
+
+namespace AdventOfCode.Core.PuzzleSolvers._2023;
+
+public class Puzzle03Solver : IPuzzleSolver
+{
+    public string SolvePartOne(bool useSample = false)
+    {
+        var dataRaw = DataReader.GetData(3, 1, useSample);
+        var asLines = dataRaw.Split(Environment.NewLine);
+        var lineLength = asLines[0].Length + Environment.NewLine.Length;
+        var lineCount = asLines.Length;
+
+        var numbers = Regex.Matches(dataRaw, @"\d+");
+        var numbersWhereMatch = numbers.Where(n => IsAdjacentToSymbol(n, dataRaw, lineLength, lineCount));
+
+        return numbersWhereMatch.Select(n => int.Parse(n.Value)).Sum().ToString();
+    }
+
+    public string SolvePartTwo(bool useSample = false)
+    {
+        var dataRaw = DataReader.GetData(3, 1, useSample);
+        var asLines = dataRaw.Split(Environment.NewLine);
+        var lineLength = asLines[0].Length + Environment.NewLine.Length;
+        var lineCount = asLines.Length;
+
+        var numbers = Regex.Matches(dataRaw, @"\d+");
+        var gears = Regex.Matches(dataRaw, @"\*");
+
+        return gears.Sum(g =>
+        {
+            var adjacentNumbers = numbers.Where(n => AdjacentSymbolIndexes(n, dataRaw, lineLength, lineCount).Contains(g.Index));
+            return adjacentNumbers.Count() == 2
+                ? int.Parse(adjacentNumbers.First().Value) * int.Parse(adjacentNumbers.Last().Value)
+                : 0;
+        }).ToString();
+    }
+
+    private static bool IsAdjacentToSymbol(Match n, string dataRaw, int lineLength, int lineCount)
+        => AdjacentSymbolIndexes(n, dataRaw, lineLength, lineCount).Any();
+
+    private static int[] AdjacentNumbers(object? gear) => Array.Empty<int>();
+
+    private static IEnumerable<int> AdjacentSymbolIndexes(Match n, string dataRaw, int lineLength, int lineCount)
+    {
+        var indexes = new List<int>();
+        for (int i = 0; i < n.Value.Length; i++)
+        {
+            var indexInData = n.Index + i;
+            var x = indexInData % lineLength;
+            var y = indexInData / lineLength;
+
+            // Look at eight neighbors
+
+            var nn = y > 0
+                ? indexInData - lineLength : -1;
+            var ne = y > 0 && x < lineLength - 1
+                ? nn + 1 : -1;
+            var nw = y > 0 && x > 0
+                ? nn - 1 : -1;
+
+            var ss = y < lineCount - 1
+                ? indexInData + lineLength : -1;
+            var se = y < lineCount - 1 && x < lineLength - 1
+                ? ss + 1 : -1;
+            var sw = y < lineCount - 1 && x > 0
+                ? ss - 1 : -1;
+
+            var ee = x < lineLength - 1
+                ? indexInData + 1 : -1;
+            var ww = x > 0
+                ? indexInData - 1 : -1;
+
+            var neighborIndices = new[] { nn, ne, ee, se, ss, sw, ww, nw }.Where(z => z >= 0);
+            indexes.AddRange(neighborIndices.Where(ix => IsSymbol(dataRaw[ix].ToString())));
+        }
+
+        return indexes.Distinct();
+    }
+
+    private static bool IsSymbol(string c) => Regex.IsMatch(c, @"[^\d\.\r\n]");
+}
--- CSharp/AdventOfCode.Tests/SolutionVerifiers.cs
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers.cs
@@ -38,4 +38,21 @@ public class SolutionVerifiers
     [TestMethod]
     public void Puzzle02_Part2()
         => Assert.AreEqual("76008", new Puzzle02Solver().SolvePartTwo());
+
+
+    [TestMethod]
+    public void Puzzle03_Part1_Sample()
+        => Assert.AreEqual("4361", new Puzzle03Solver().SolvePartOne(useSample: true));
+
+    [TestMethod]
+    public void Puzzle03_Part1()
+        => Assert.AreEqual("540131", new Puzzle03Solver().SolvePartOne());
+
+    [TestMethod]
+    public void Puzzle03_Part2_Sample()
+        => Assert.AreEqual("467835", new Puzzle03Solver().SolvePartTwo(useSample: true));
+
+    [TestMethod]
+    public void Puzzle03_Part2()
+        => Assert.AreEqual("86879020", new Puzzle03Solver().SolvePartTwo());
 }
