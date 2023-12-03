4ac7bbdbb1821fb9af908fc831cc7a1e6c69be45
Andrew <5436387+fidwell@users.noreply.github.com>
2023-12-03 11:39:56 -0500

    Test refactor

--- CSharp/AdventOfCode.Tests/SolutionVerifiers.cs
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers.cs
@@ -6,53 +6,39 @@ namespace AdventOfCode.Tests;
 [TestClass]
 public class SolutionVerifiers
 {
-    [TestMethod]
-    public void Puzzle01_Part1_Sample()
-        => Assert.AreEqual("142", new Puzzle01Solver().SolvePartOne(useSample: true));
+    [DataTestMethod]
+    [DataRow(true, "142")]
+    [DataRow(false, "54632")]
+    public void Puzzle01_Part1(bool useSample, string expected)
+        => Assert.AreEqual(expected, new Puzzle01Solver().SolvePartOne(useSample));
 
-    [TestMethod]
-    public void Puzzle01_Part1()
-        => Assert.AreEqual("54632", new Puzzle01Solver().SolvePartOne());
+    [DataTestMethod]
+    [DataRow(true, "281")]
+    [DataRow(false, "54019")]
+    public void Puzzle01_Part2(bool useSample, string expected)
+        => Assert.AreEqual(expected, new Puzzle01Solver().SolvePartTwo(useSample));
 
-    [TestMethod]
-    public void Puzzle01_Part2_Sample()
-        => Assert.AreEqual("281", new Puzzle01Solver().SolvePartTwo(useSample: true));
+    [DataTestMethod]
+    [DataRow(true, "8")]
+    [DataRow(false, "2348")]
+    public void Puzzle02_Part1(bool useSample, string expected)
+        => Assert.AreEqual(expected, new Puzzle02Solver().SolvePartOne(useSample));
 
-    [TestMethod]
-    public void Puzzle01_Part2()
-        => Assert.AreEqual("54019", new Puzzle01Solver().SolvePartTwo());
+    [DataTestMethod]
+    [DataRow(true, "2286")]
+    [DataRow(false, "76008")]
+    public void Puzzle02_Part2(bool useSample, string expected)
+        => Assert.AreEqual(expected, new Puzzle02Solver().SolvePartTwo(useSample));
 
+    [DataTestMethod]
+    [DataRow(true, "4361")]
+    [DataRow(false, "540131")]
+    public void Puzzle03_Part1(bool useSample, string expected)
+        => Assert.AreEqual(expected, new Puzzle03Solver().SolvePartOne(useSample));
 
-    [TestMethod]
-    public void Puzzle02_Part1_Sample()
-        => Assert.AreEqual("8", new Puzzle02Solver().SolvePartOne(useSample: true));
-
-    [TestMethod]
-    public void Puzzle02_Part1()
-        => Assert.AreEqual("2348", new Puzzle02Solver().SolvePartOne());
-
-    [TestMethod]
-    public void Puzzle02_Part2_Sample()
-        => Assert.AreEqual("2286", new Puzzle02Solver().SolvePartTwo(useSample: true));
-
-    [TestMethod]
-    public void Puzzle02_Part2()
-        => Assert.AreEqual("76008", new Puzzle02Solver().SolvePartTwo());
-
-
-    [TestMethod]
-    public void Puzzle03_Part1_Sample()
-        => Assert.AreEqual("4361", new Puzzle03Solver().SolvePartOne(useSample: true));
-
-    [TestMethod]
-    public void Puzzle03_Part1()
-        => Assert.AreEqual("540131", new Puzzle03Solver().SolvePartOne());
-
-    [TestMethod]
-    public void Puzzle03_Part2_Sample()
-        => Assert.AreEqual("467835", new Puzzle03Solver().SolvePartTwo(useSample: true));
-
-    [TestMethod]
-    public void Puzzle03_Part2()
-        => Assert.AreEqual("86879020", new Puzzle03Solver().SolvePartTwo());
+    [DataTestMethod]
+    [DataRow(true, "467835")]
+    [DataRow(false, "86879020")]
+    public void Puzzle03_Part2(bool useSample, string expected)
+        => Assert.AreEqual(expected, new Puzzle03Solver().SolvePartTwo(useSample));
 }
