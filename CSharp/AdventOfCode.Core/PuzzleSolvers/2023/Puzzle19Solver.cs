e20f148ccf760ab99f490ec071729f70be99cfc3
Andrew <5436387+fidwell@users.noreply.github.com>
2023-12-13 16:30:34 -0500

    Adding placeholder puzzle solvers

---
+++ CSharp/AdventOfCode.Core/PuzzleSolvers/2023/Puzzle19Solver.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode.Core.PuzzleSolvers._2023;
+
+public class Puzzle19Solver : IPuzzleSolver
+{
+    public string SolvePartOne(bool useSample = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string SolvePartTwo(bool useSample = false)
+    {
+        throw new NotImplementedException();
+    }
+}
