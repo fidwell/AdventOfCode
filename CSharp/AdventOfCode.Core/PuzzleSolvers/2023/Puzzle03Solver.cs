e234548b2aa4526bf36068e2298e1736a2102545
Andrew <5436387+fidwell@users.noreply.github.com>
2023-12-17 14:49:03 -0500

    2023 puzzle 3 micro-optimizations

--- CSharp/AdventOfCode.Core/PuzzleSolvers/2023/Puzzle03Solver.cs
+++ CSharp/AdventOfCode.Core/PuzzleSolvers/2023/Puzzle03Solver.cs
@@ -1,15 +1,18 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 using AdventOfCode.Core.StringUtilities;
 
 namespace AdventOfCode.Core.PuzzleSolvers._2023;
 
 public class Puzzle03Solver : IPuzzleSolver
 {
+    private static readonly Regex NumberRegex = new(@"\d+", RegexOptions.Compiled);
+
     public string SolvePartOne(string input)
     {
         var matrix = new CharacterMatrix(input);
         return matrix
-            .FindAllWords(new Regex(@"\d+"))
+            .FindAllWords(NumberRegex)
             .Where(n => matrix.CoordinatesOfNeighbors(n).Any(coordinate => IsSymbol(matrix.CharAt(coordinate))))
             .Select(n => int.Parse(matrix.StringAt(n.StartCoordinate, n.Length)))
             .Sum()
@@ -19,20 +22,35 @@ public class Puzzle03Solver : IPuzzleSolver
     public string SolvePartTwo(string input)
     {
         var matrix = new CharacterMatrix(input);
-        var numbers = matrix.FindAllWords(new Regex(@"\d+"));
+        var numbers = matrix.FindAllWords(NumberRegex);
+        var numbersWithNeighbors = numbers.Select(n => new
+        {
+            Number = n,
+            Neighbors = matrix.CoordinatesOfNeighbors(n)
+        }).ToList();
 
         return matrix
             .FindAllCharacters('*')
             .Select(g =>
             {
-                var neighboringWords = numbers.Where(n => matrix.CoordinatesOfNeighbors(n).Contains(g));
-                return neighboringWords.Count() == 2
-                    ? neighboringWords.Select(w => int.Parse(w.Value)).Aggregate((a, b) => a * b)
+                var neighboringWords = numbersWithNeighbors.Where(n => n.Neighbors.Contains(g)).ToList();
+                return neighboringWords.Count == 2
+                    ? neighboringWords.Select(w => int.Parse(w.Number.Value)).Aggregate((a, b) => a * b)
                     : 0;
             })
             .Sum()
             .ToString();
     }
 
-    private static bool IsSymbol(char c) => Regex.IsMatch($"{c}", @"[^\d\.\r\n]");
+    private static bool IsSymbol(char c) =>
+        c == '+' ||
+        c == '*' ||
+        c == '=' ||
+        c == '-' ||
+        c == '&' ||
+        c == '#' ||
+        c == '/' ||
+        c == '%' ||
+        c == '$' ||
+        c == '@';
 }
