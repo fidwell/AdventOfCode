6680cb53db22d3193e6b3324c17a1a7f12fa60ce
Andrew <5436387+fidwell@users.noreply.github.com>
2023-12-18 19:37:35 -0500

    2022 puzzle 2

--- CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2022.cs
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2022.cs
@@ -12,7 +12,8 @@ public class SolutionVerifier2022 : SolutionVerifier
     public SolutionVerifier2022()
     {
         _solvers = [
-            new Puzzle01Solver()
+            new Puzzle01Solver(),
+            new Puzzle02Solver(),
         ];
     }
 
@@ -21,6 +22,11 @@ public class SolutionVerifier2022 : SolutionVerifier
     [DataRow(1, 1, false, "71471", DisplayName = "2022.01.1")]
     [DataRow(1, 2, true, "45000", DisplayName = "2022.01.2-s")]
     [DataRow(1, 2, false, "211189", DisplayName = "2022.01.2")]
+
+    [DataRow(2, 1, true, "15", DisplayName = "2022.02.1-s")]
+    [DataRow(2, 1, false, "11841", DisplayName = "2022.02.1")]
+    [DataRow(2, 2, true, "12", DisplayName = "2022.02.2-s")]
+    [DataRow(2, 2, false, "13022", DisplayName = "2022.02.2")]
     public void Solve(int puzzle, int part, bool useSample, string expected) =>
         Solve(_solvers[puzzle - 1], 2022, puzzle, part, useSample, expected);
 }
