fe39b408167084c67ddb2bf6fdeac3de2c738db1
Andrew <5436387+fidwell@users.noreply.github.com>
2023-12-19 13:16:38 -0500

    2023 puzzle 19 part 2

--- CSharp/AdventOfCode.Core/PuzzleSolvers/2023/Puzzle19Solver.cs
+++ CSharp/AdventOfCode.Core/PuzzleSolvers/2023/Puzzle19Solver.cs
@@ -47,7 +47,49 @@ public class Puzzle19Solver : IPuzzleSolver
         return acceptedPartSum.ToString();
     }
 
-    public string SolvePartTwo(string input) => throw new NotImplementedException();
+    public string SolvePartTwo(string input)
+    {
+        var workflows = new Dictionary<string, IEnumerable<WorkflowRule>>();
+
+        foreach (var line in input.Split(Environment.NewLine))
+        {
+            if (line.Length > 0 && line[0] != '{')
+            {
+                var w = ParseWorkflow(line);
+                workflows.Add(w.Item1, w.Item2);
+            }
+        }
+
+        var startingRange = new PartValueRange(new Dictionary<char, Range>
+        {
+            { 'x', new Range(1, 4000) },
+            { 'm', new Range(1, 4000) },
+            { 'a', new Range(1, 4000) },
+            { 's', new Range(1, 4000) }
+        });
+        var unfinishedRanges = new Queue<RangeResult>();
+        unfinishedRanges.Enqueue(new RangeResult("in", startingRange));
+
+        var acceptedRanges = new List<PartValueRange>();
+        while (unfinishedRanges.Count > 0)
+        {
+            var thisRange = unfinishedRanges.Dequeue();
+            var resultingRanges = ApplyWorkflow(thisRange.PartValueRange, workflows[thisRange.ResultingWorkflow]).ToList();
+            foreach (var range in resultingRanges)
+            {
+                if (range.ResultingWorkflow == "A")
+                {
+                    acceptedRanges.Add(range.PartValueRange);
+                }
+                else if (range.ResultingWorkflow != "R")
+                {
+                    unfinishedRanges.Enqueue(range);
+                }
+            }
+        }
+
+        return acceptedRanges.Sum(r => r.TotalValues).ToString();
+    }
 
     private static (string, IEnumerable<WorkflowRule>) ParseWorkflow(string line)
     {
@@ -96,10 +138,37 @@ public class Puzzle19Solver : IPuzzleSolver
         return string.Empty;
     }
 
+    private static IEnumerable<RangeResult> ApplyWorkflow(PartValueRange partValueRange, IEnumerable<WorkflowRule> rules)
+    {
+        var ranges = new Queue<PartValueRange>();
+        ranges.Enqueue(partValueRange);
+
+        foreach (var rule in rules)
+        {
+            var thisRange = ranges.Dequeue();
+
+            var results = rule.Apply(thisRange).ToList();
+            foreach (var result in results)
+            {
+                if (!string.IsNullOrWhiteSpace(result.ResultingWorkflow))
+                {
+                    yield return result;
+                }
+                else
+                {
+                    ranges.Enqueue(result.PartValueRange);
+                }
+            }
+        }
+    }
+
+    #region Classes
+
     private abstract class WorkflowRule(string target)
     {
         public readonly string Target = target;
         public abstract string Apply(Part part);
+        public abstract IEnumerable<RangeResult> Apply(PartValueRange partValueRange);
     }
 
     private class ConditionalRule(char parameter, bool isLessThan, int amount, string target)
@@ -118,12 +187,37 @@ public class Puzzle19Solver : IPuzzleSolver
                 return Target;
             return string.Empty;
         }
+
+        public override IEnumerable<RangeResult> Apply(PartValueRange partValueRange)
+        {
+            var rangeInQuestion = partValueRange.RatingRanges[Parameter];
+            var unaffectedRatings = partValueRange.RatingRanges.Where(r => r.Key != Parameter);
+
+            var lowerRangeRatings = unaffectedRatings.ToDictionary(e => e.Key, e => e.Value);
+            var lowerRatingRange = IsLessThan
+                ? new Range(rangeInQuestion.Start, Amount - rangeInQuestion.Start)
+                : new Range(rangeInQuestion.Start, Amount - rangeInQuestion.Start + 1);
+            lowerRangeRatings.Add(Parameter, lowerRatingRange);
+            yield return new RangeResult(
+                IsLessThan ? Target : string.Empty,
+                new PartValueRange(lowerRangeRatings));
+
+            var higherRangeRatings = unaffectedRatings.ToDictionary(e => e.Key, e => e.Value);
+            var higherRatingRange = IsLessThan
+                ? new Range(Amount, rangeInQuestion.End - Amount)
+                : new Range(Amount + 1, rangeInQuestion.End - Amount - 1);
+            higherRangeRatings.Add(Parameter, higherRatingRange);
+            yield return new RangeResult(
+                IsLessThan ? string.Empty : Target,
+                new PartValueRange(higherRangeRatings));
+        }
     }
 
     private class FinalRule(string target)
         : WorkflowRule(target)
     {
         public override string Apply(Part part) => Target;
+        public override IEnumerable<RangeResult> Apply(PartValueRange partValueRange) => [new(Target, partValueRange)];
     }
 
     private class Part(Dictionary<char, int> values)
@@ -131,4 +225,18 @@ public class Puzzle19Solver : IPuzzleSolver
         public readonly Dictionary<char, int> Ratings = values;
         public int Sum => Ratings.Sum(r => r.Value);
     }
+
+    private class PartValueRange(Dictionary<char, Range> ratingRanges)
+    {
+        public readonly Dictionary<char, Range> RatingRanges = ratingRanges;
+        public long TotalValues => RatingRanges.Aggregate(1L, (a, b) => a * b.Value.Length);
+    }
+
+    private class RangeResult(string resultingWorkflow, PartValueRange partValueRange)
+    {
+        public string ResultingWorkflow = resultingWorkflow;
+        public PartValueRange PartValueRange = partValueRange;
+    }
+
+    #endregion
 }
--- CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2023.cs
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2023.cs
@@ -133,8 +133,8 @@ public class SolutionVerifier2023 : SolutionVerifier
 
     [DataRow(19, 1, true, "19114", DisplayName = "2023.19.1-s")]
     [DataRow(19, 1, false, "432434", DisplayName = "2023.19.1")]
-    [DataRow(19, 2, true, "_", DisplayName = "2023.19.2-s")]
-    [DataRow(19, 2, false, "_", DisplayName = "2023.19.2")]
+    [DataRow(19, 2, true, "167409079868000", DisplayName = "2023.19.2-s")]
+    [DataRow(19, 2, false, "132557544578569", DisplayName = "2023.19.2")]
 
     [DataRow(20, 1, true, "_", DisplayName = "2023.20.1-s")]
     [DataRow(20, 1, false, "_", DisplayName = "2023.20.1")]
