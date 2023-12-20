486b50deb942a80c96ab33351a2a5bc55f45decc
Andrew <5436387+fidwell@users.noreply.github.com>
2023-12-20 09:40:40 -0500

    Some setup

--- CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2023.cs
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2023.cs
@@ -136,7 +136,7 @@ public class SolutionVerifier2023 : SolutionVerifier
     [DataRow(19, 2, true, "167409079868000", DisplayName = "2023.19.2-s")]
     [DataRow(19, 2, false, "132557544578569", DisplayName = "2023.19.2")]
 
-    [DataRow(20, 1, true, "_", DisplayName = "2023.20.1-s")]
+    [DataRow(20, 1, true, "32000000", DisplayName = "2023.20.1-s")]
     [DataRow(20, 1, false, "_", DisplayName = "2023.20.1")]
     [DataRow(20, 2, true, "_", DisplayName = "2023.20.2-s")]
     [DataRow(20, 2, false, "_", DisplayName = "2023.20.2")]
