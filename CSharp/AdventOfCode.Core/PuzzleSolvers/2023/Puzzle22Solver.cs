3d946f8fb09ee84a4b0ecd988051ed76546e23ce
Andrew <5436387+fidwell@users.noreply.github.com>
2023-12-22 17:48:57 -0500

    new algorithm setup

--- CSharp/AdventOfCode.Core/PuzzleSolvers/2023/Puzzle22Solver.cs
+++ CSharp/AdventOfCode.Core/PuzzleSolvers/2023/Puzzle22Solver.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Diagnostics;
+using System.Threading.Tasks.Sources;
 
 namespace AdventOfCode.Core.PuzzleSolvers._2023;
 
@@ -8,66 +10,64 @@ public class Puzzle22Solver : IPuzzleSolver
     public string SolvePartOne(string input)
     {
         var bricks = input.Split(Environment.NewLine)
-            //.Take(100)
             .Select((l, i) => new Brick(l, i))
+            .OrderBy(b => b.Point1.Z)
             .ToList();
-            //.ToDictionary(d => d.Id, d => d);
 
+        // Not general, but this is my input
+        const int maxX = 9;
+        const int maxY = 9;
+        var occupiedZs =
+            Enumerable.Range(0, maxX).SelectMany(x =>
+            Enumerable.Range(0, maxY).Select(y =>
+                (x, y, 0)))
+            .ToList();
+
+        // Part 1. Fall
         foreach (var brick in bricks)
         {
-            brick.BricksUnderThis = bricks.Where(b => b.AllCubes.Any(thatCube =>
-                brick.AllCubes.Any(thisCube =>
-                    thatCube.X == thisCube.X &&
-                    thatCube.Y == thisCube.Y &&
-                    thatCube.Z < thisCube.Z))).Select(b => b.Id).ToList();
+            // Spaces under this cube are all elements in occupiedZs (oz) where
+            // oz.X is in allcubes.x and oz.Y is in allcubes.y.
+            // We can fall down to one more than the max Z in that list.
+            var spacesUnder = occupiedZs.Where(oz =>
+                brick.AllCubes.Select(c => c.X).Contains(oz.x) &&
+                brick.AllCubes.Select(c => c.Y).Contains(oz.y));
+            var maxZ = spacesUnder.Max(oz => oz.Item3);
+            var minZOfThis = brick.Point1.Z; // should be enforced by brick constructor
+            var fallAmount = minZOfThis - maxZ - 1;
+            brick.Fall(fallAmount);
+
+            foreach (var p in brick.AllCubes)
+            {
+                var occupiedZHere = occupiedZs.Single(oz => oz.x == p.X && oz.y == p.Y);
+                occupiedZs.Remove(occupiedZHere);
+                occupiedZs.Add((p.X, p.Y, Math.Max(occupiedZHere.Item3, p.Z)));
+            }
         }
 
-        var stableBrickIds = new HashSet<int>();
-        while (stableBrickIds.Count < bricks.Count)
+        // 2. Create dependency graph
+        var dependencyGraph = new Dictionary<int, List<int>>();
+        for (var i = 0; i < bricks.Count; i++)
         {
-            foreach (var brick in bricks)
+            var thisBrick = bricks[i];
+            for (var j = i + 1; j < bricks.Count; j++)
             {
-                if (stableBrickIds.Contains(brick.Id))
-                    continue;
-
-                if (brick.Point1.Z == 1 || brick.Point2.Z == 1)
+                var thatBrick = bricks[j];
+                if (Supports(thisBrick, thatBrick))
                 {
-                    stableBrickIds.Add(brick.Id);
-                    continue;
-                }
-
-                while (!IsSupported(brick, bricks))
-                {
-                    brick.Fall();
-                }
-
-                var supporters = Supporters(brick, bricks);
-                if (supporters.Any(s => stableBrickIds.Contains(s.Id)))
-                {
-                    stableBrickIds.Add(brick.Id);
+                    if (!dependencyGraph.ContainsKey(thisBrick.Id))
+                    {
+                        dependencyGraph.Add(thisBrick.Id, []);
+                    }
+                    dependencyGraph[thisBrick.Id].Add(thatBrick.Id);
                 }
             }
         }
 
-        return bricks.Count(brick =>
-        {
-            var newBricks = bricks.Where(b => b != brick).ToList();
-            return newBricks.All(b => IsSupported(b, newBricks));
-        }).ToString();
+        return "";
     }
 
     public string SolvePartTwo(string input) => throw new NotImplementedException();
-
-    private static bool IsSupported(Brick brick, List<Brick> bricks)
-    {
-        if (brick.Point1.Z == 1 || brick.Point2.Z == 1)
-            return true;
-        var bricksUnder = bricks.Where(b => brick.BricksUnderThis.Contains(b.Id));
-        return bricksUnder.Any(a => a != brick && Supports(a, brick));
-    }
-
-    private static IEnumerable<Brick> Supporters(Brick brick, List<Brick> bricks) =>
-        bricks.Where(a => a != brick && Supports(a, brick));
 
     private static bool Supports(Brick lower, Brick higher) =>
         lower.AllCubes.Any(lc => higher.AllCubes.Any(hc => 
@@ -81,30 +81,54 @@ public class Puzzle22Solver : IPuzzleSolver
         public readonly Point3d Point1;
         public readonly Point3d Point2;
         public readonly Point3d[] AllCubes;
-        public List<int> BricksUnderThis;
 
         public Brick(string input, int id)
         {
             Id = id;
             var portions = input.Split('~');
-            Point1 = new Point3d(portions.First());
-            Point2 = new Point3d(portions.Last());
-            AllCubes =
-                Enumerable.Range(Point1.X, Point2.X - Point1.X + 1).SelectMany(x =>
-                Enumerable.Range(Point1.Y, Point2.Y - Point1.Y + 1).SelectMany(y =>
-                Enumerable.Range(Point1.Z, Point2.Z - Point1.Z + 1).Select(z =>
-                    new Point3d(x, y, z)))).ToArray();
-            BricksUnderThis = [];
+            var p1 = new Point3d(portions.First());
+            var p2 = new Point3d(portions.Last());
+            Point1 = p1.Z < p2.Z ? p1 : p2;
+            Point2 = Point1 == p1 ? p2 : p1;
+            AllCubes = GetAllPoints().ToArray();
         }
 
         public char IdChar => (char)('A' + Id);
 
-        public void Fall()
+        public void Fall(int amount)
         {
-            Point1.Z--;
-            Point2.Z--;
+            Point1.Z -= amount;
+            Point2.Z -= amount;
             foreach (var point in AllCubes)
-                point.Z--;
+            {
+                point.Z -= amount;
+            }
+        }
+
+        private IEnumerable<Point3d> GetAllPoints()
+        {
+            if (Point1.X != Point2.X)
+            {
+                var start = Point1.X < Point2.X ? Point1.X : Point2.X;
+                var end = Point1.X > Point2.X ? Point1.X : Point2.X;
+                var count = end - start + 1;
+                return Enumerable.Range(start, count).Select(x => new Point3d(x, Point1.Y, Point1.Z));
+            }
+            else if (Point1.Y != Point2.Y)
+            {
+                var start = Point1.Y < Point2.Y ? Point1.Y : Point2.Y;
+                var end = Point1.Y > Point2.Y ? Point1.Y : Point2.Y;
+                var count = end - start + 1;
+                return Enumerable.Range(start, count).Select(y => new Point3d(Point1.X, y, Point1.Z));
+            }
+            else if (Point1.Z != Point2.Z)
+            {
+                var start = Point1.Z < Point2.Z ? Point1.Z : Point2.Z;
+                var end = Point1.Z > Point2.Z ? Point1.Z : Point2.Z;
+                var count = end - start + 1;
+                return Enumerable.Range(start, count).Select(z => new Point3d(Point1.X, Point1.Y, z));
+            }
+            throw new NotImplementedException();
         }
     }
 
