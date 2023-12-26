89bfc480961e83d1bda9ccb5c1948a27cce78964
Andrew <5436387+fidwell@users.noreply.github.com>
2023-12-25 22:12:20 -0500

    Cleanup

--- Python/solvers2022/solver07.py
+++ Python/solvers2022/solver07.py
@@ -1,29 +1,18 @@
 def solve_part1(data):
-    directories = get_directories(data)
-    return str(sum(directories[d] for d in directories if directories[d] < 100000))
+    return str(sum(size for (_, size) in get_directories(data).items() if size < 100000))
 
 def solve_part2(data):
-    total_space = 70000000
-    required_space = 30000000
     directories = get_directories(data)
-
-    used_space = directories[""]
-    unused_space = total_space - used_space
-    needed_space = required_space - unused_space
-    print(needed_space)
-
+    needed_space = 30000000 - (70000000 - directories[""])
     min_amount = directories[""]
-
     for (_, size) in directories.items():
         if needed_space <= size <= min_amount:
             min_amount = size
-
     return str(min_amount)
 
 def get_directories(data):
     long_paths = {"": 0}
     directories = {}
-
     current_path = ""
     for line in data[1:]:
         if line[0] == "$":
@@ -44,9 +33,13 @@ def get_directories(data):
                 size = int(portions[0])
                 long_paths[current_path + "/" + file_name] = size
 
-    for (name, size) in long_paths.items():
+    path_items = long_paths.items()
+    for (name, size) in path_items:
         if size == 0:
-            total_size = sum(long_paths[p] for p in long_paths if len(p) > len(name) and p.startswith(name))
+            total_size = sum(size0 for (name0, size0) in path_items if is_in(name0, name))
             directories[name] = total_size
 
     return directories
+
+def is_in(directory, parent):
+    return len(directory) > len(parent) and directory.startswith(parent)
