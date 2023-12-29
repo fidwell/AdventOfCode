77127235c86fc40e7e024dff83deaa748243ef7a
Andrew <5436387+fidwell@users.noreply.github.com>
2023-12-28 20:07:52 -0500

    2022 puzzle 12 part 1

---
+++ Python/solvers/_2022/solver12.py
@@ -0,0 +1,104 @@
+from queue import PriorityQueue
+
+
+def solve_part1(data):
+    matrix = [list(list(line)) for line in data]
+    start = find_char(matrix, "S")
+    end = find_char(matrix, "E")
+    set_value(matrix, start, "a")
+    set_value(matrix, end, "z")
+    result = a_star(matrix, start, end)
+    return str(len(result) - 1)
+
+
+def solve_part2(data):
+    return "_"
+
+
+def find_char(matrix, target):
+    for y, row in enumerate(matrix):
+        for x, value in enumerate(row):
+            if value == target:
+                return (x, y)
+    raise ValueError("Couldn't find start")
+
+
+def a_star(matrix, start, end):
+    start_node = Node(None, start)
+    start_node.total_cost = start_node.current_to_start = start_node.heuristic = 0
+    end_node = Node(None, end)
+    end_node.total_cost = end_node.current_to_start = end_node.heuristic = 0
+
+    visited = set([])
+    queue = PriorityQueue()
+    queue.put((0, start_node))
+
+    while queue.qsize() > 0:
+        (_, current_node) = queue.get()
+        visited.add(current_node.position)
+
+        if current_node.position == end:
+            print("Found end!!")
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1]
+
+        neighbors = neighbors_of(matrix, current_node.position)
+        for n in neighbors:
+            new_node = Node(current_node, position=n)
+            if new_node.position in visited:
+                continue
+            new_node.current_to_start = current_node.current_to_start + 1
+            new_node.heuristic = (
+                (new_node.position[0] - end_node.position[0]) ** 2
+            ) + ((new_node.position[1] - end_node.position[1]) ** 2)
+            new_node.total_cost = new_node.current_to_start + new_node.heuristic
+
+            if not any(new_node.position == q.position for (_, q) in queue.queue):
+                queue.put((new_node.current_to_start, new_node))
+
+    return None
+
+
+def neighbors_of(matrix, location):
+    value_here = value_at(matrix, location)
+
+    result = []
+    x = location[0]
+    y = location[1]
+    if x > 0:
+        result.append((x - 1, y))
+    if y > 0:
+        result.append((x, y - 1))
+    if x < len(matrix[0]) - 1:
+        result.append((x + 1, y))
+    if y < len(matrix) - 1:
+        result.append((x, y + 1))
+
+    return list(n for n in result if ord(value_at(matrix, n)) <= ord(value_here) + 1)
+
+
+def value_at(matrix, location):
+    return matrix[location[1]][location[0]]
+
+
+def set_value(matrix, location, value):
+    matrix[location[1]][location[0]] = value
+
+
+class Node:
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+        self.total_cost = 0
+        self.current_to_start = 0
+        self.heuristic = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+    def __lt__(self, other):
+        return self.position[0] < other.position[0]
---
+++ Python/tests/_2022/test_2022_solver12.py
@@ -0,0 +1,21 @@
+import unittest
+from solvers._2022 import solver12
+from tests import datareader
+
+
+class TestSolver202212(unittest.TestCase):
+    def test_solver12_part1_example(self):
+        data = datareader.read_lines(2022, 12, 1, True)
+        self.assertEqual(solver12.solve_part1(data), "31")
+
+    def test_solver12_part1(self):
+        data = datareader.read_lines(2022, 12, 1, False)
+        self.assertEqual(solver12.solve_part1(data), "520")
+
+    def test_solver12_part2_example(self):
+        data = datareader.read_lines(2022, 12, 2, True)
+        self.assertEqual(solver12.solve_part2(data), "_")
+
+    def test_solver12_part2(self):
+        data = datareader.read_lines(2022, 12, 2, False)
+        self.assertEqual(solver12.solve_part2(data), "_")
