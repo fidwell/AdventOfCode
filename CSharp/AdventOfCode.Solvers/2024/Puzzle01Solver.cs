eee17ac04dd6846a9c6debede1e3d9af1996ad68
Andrew <5436387+fidwell@users.noreply.github.com>
2024-11-29 15:09:23 -0500

    Prep for 2024 part 1

---
+++ CSharp/AdventOfCode.Solvers/2024/Puzzle01Solver.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode.Solvers._2024;
+
+public class Puzzle01Solver : IPuzzleSolver
+{
+	public string SolvePartOne(string input) => "X";
+
+	public string SolvePartTwo(string input) => "X";
+}
---
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2024.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode.Solvers._2024;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Tests.SolutionVerifiers;
+
+[TestClass]
+public class SolutionVerifier2024 : SolutionVerifier
+{
+	public SolutionVerifier2024() : base(2024) { }
+
+	[DataTestMethod, Timeout(Timeout)]
+	[DataRow(1, true, "X", DisplayName = "2024.01.1-e")]
+	[DataRow(1, false, "X", DisplayName = "2024.01.1-r")]
+	[DataRow(2, true, "X", DisplayName = "2024.01.2-e")]
+	[DataRow(2, false, "X", DisplayName = "2024.01.2-r")]
+	public void Solve_2024_01(int part, bool useExample, string expected) =>
+		Solve(new Puzzle01Solver(), 1, part, useExample, expected);
+}
