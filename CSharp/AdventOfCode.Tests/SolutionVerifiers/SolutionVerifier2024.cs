9a633419601f52e133b6b61b9086370be797b30f
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-01 00:14:20 -0500

    2024 puzzle 1

--- CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2024.cs
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2024.cs
@@ -9,10 +9,10 @@ public class SolutionVerifier2024 : SolutionVerifier
 	public SolutionVerifier2024() : base(2024) { }
 
 	[DataTestMethod, Timeout(Timeout)]
-	[DataRow(1, true, "X", DisplayName = "2024.01.1-e")]
-	[DataRow(1, false, "X", DisplayName = "2024.01.1-r")]
-	[DataRow(2, true, "X", DisplayName = "2024.01.2-e")]
-	[DataRow(2, false, "X", DisplayName = "2024.01.2-r")]
+	[DataRow(1, true, "11", DisplayName = "2024.01.1-e")]
+	[DataRow(1, false, "2375403", DisplayName = "2024.01.1-r")]
+	[DataRow(2, true, "31", DisplayName = "2024.01.2-e")]
+	[DataRow(2, false, "23082277", DisplayName = "2024.01.2-r")]
 	public void Solve_2024_01(int part, bool useExample, string expected) =>
 		Solve(new Puzzle01Solver(), 1, part, useExample, expected);
 }
