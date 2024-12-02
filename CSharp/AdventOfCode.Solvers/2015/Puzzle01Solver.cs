64bd3fc84ae2766c891a3f65319c967cdf87deeb
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-02 13:22:51 -0500

    Refactor 2015 puzzle 1

--- CSharp/AdventOfCode.Solvers/2015/Puzzle01Solver.cs
+++ CSharp/AdventOfCode.Solvers/2015/Puzzle01Solver.cs
@@ -1,29 +1,21 @@
-﻿using System.Text.RegularExpressions;
-
-namespace AdventOfCode.Solvers._2015;
+﻿namespace AdventOfCode.Solvers._2015;
 
 public class Puzzle01Solver : IPuzzleSolver
 {
-	public string SolvePartOne(string input)
-	{
-		var opens = Regex.Matches(input, @"\(");
-		var closes = Regex.Matches(input, @"\)");
-		return (opens.Count - closes.Count).ToString();
-	}
+    public string SolvePartOne(string input) =>
+        (2 * input.Count(c => c == '(') - input.Length).ToString();
 
-	public string SolvePartTwo(string input)
-	{
-		var floor = 0;
+    public string SolvePartTwo(string input)
+    {
+        var floor = 0;
 
-		for (int i = 0; i < input.Length; i++)
-		{
-			floor += input[i] == '(' ? 1 : -1;
-			if (floor == -1)
-			{
-				return (i + 1).ToString();
-			}
-		}
+        for (var i = 0; i < input.Length; i++)
+        {
+            floor += input[i] == '(' ? 1 : -1;
+            if (floor == -1)
+                return (i + 1).ToString();
+        }
 
-		throw new Exception("Couldn't find the answer");
-	}
+        throw new Exception("Couldn't find the answer");
+    }
 }
