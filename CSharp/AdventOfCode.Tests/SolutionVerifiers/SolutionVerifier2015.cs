2a170d60b24b87dad0e205f030b28776058920e7
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-01 22:28:27 -0500

    2015 puzzle 1

---
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2015.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode.Solvers._2015;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Tests.SolutionVerifiers;
+
+[TestClass]
+public class SolutionVerifier2015 : SolutionVerifier
+{
+	public SolutionVerifier2015() : base(2015) { }
+
+	[DataTestMethod, Timeout(Timeout)]
+	[DataRow(1, false, "232", DisplayName = "2015.01.1-r")]
+	[DataRow(2, false, "1783", DisplayName = "2015.01.2-r")]
+	public void Solve_2015_01(int part, bool useExample, string expected) =>
+		Solve(new Puzzle01Solver(), 1, part, useExample, expected);
+}
