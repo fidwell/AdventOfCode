27095f01f8079e08ba26784d64ecd0db39477352
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-03 14:38:02 -0500

    2015 puzzle 4

---
+++ CSharp/AdventOfCode.Solvers/2015/Puzzle04Solver.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+namespace AdventOfCode.Solvers._2015;
+
+public class Puzzle04Solver : IPuzzleSolver
+{
+    public string SolvePartOne(string input) => Solve(input, true);
+
+    public string SolvePartTwo(string input) => Solve(input, false);
+
+    private static string Solve(string input, bool isPartOne)
+    {
+        for (var i = 0; i < int.MaxValue; i++)
+        {
+            var tryInput = $"{input}{i}".ToCharArray().Select(c => (byte)c).ToArray();
+            var hash = MD5.HashData(tryInput) ?? [];
+
+            if (hash[0] == 0 &&
+                hash[1] == 0 &&
+                (isPartOne ? (hash[2] <= 15) : (hash[2] == 0)))
+            {
+                return i.ToString();
+            }
+        }
+
+        throw new Exception("No answer could be found.");
+    }
+}
--- CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2015.cs
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2015.cs
@@ -6,23 +6,29 @@ namespace AdventOfCode.Tests.SolutionVerifiers;
 [TestClass]
 public class SolutionVerifier2015 : SolutionVerifier
 {
-	public SolutionVerifier2015() : base(2015) { }
+    public SolutionVerifier2015() : base(2015) { }
 
-	[DataTestMethod, Timeout(Timeout)]
-	[DataRow(1, false, "232", DisplayName = "2015.01.1-problem")]
-	[DataRow(2, false, "1783", DisplayName = "2015.01.2-problem")]
-	public void Solve_2015_01(int part, bool useExample, string expected) =>
-		Solve(new Puzzle01Solver(), 1, part, useExample, expected);
+    [DataTestMethod, Timeout(Timeout)]
+    [DataRow(1, false, "232", DisplayName = "2015.01.1-problem")]
+    [DataRow(2, false, "1783", DisplayName = "2015.01.2-problem")]
+    public void Solve_2015_01(int part, bool useExample, string expected) =>
+        Solve(new Puzzle01Solver(), 1, part, useExample, expected);
 
-	[DataTestMethod, Timeout(Timeout)]
-	[DataRow(1, false, "1588178", DisplayName = "2015.02.1-problem")]
-	[DataRow(2, false, "3783758", DisplayName = "2015.02.2-problem")]
-	public void Solve_2015_02(int part, bool useExample, string expected) =>
-		Solve(new Puzzle02Solver(), 2, part, useExample, expected);
+    [DataTestMethod, Timeout(Timeout)]
+    [DataRow(1, false, "1588178", DisplayName = "2015.02.1-problem")]
+    [DataRow(2, false, "3783758", DisplayName = "2015.02.2-problem")]
+    public void Solve_2015_02(int part, bool useExample, string expected) =>
+        Solve(new Puzzle02Solver(), 2, part, useExample, expected);
 
-	[DataTestMethod, Timeout(Timeout)]
-	[DataRow(1, false, "2565", DisplayName = "2015.03.1-problem")]
-	[DataRow(2, false, "2639", DisplayName = "2015.03.2-problem")]
-	public void Solve_2015_03(int part, bool useExample, string expected) =>
-		Solve(new Puzzle03Solver(), 3, part, useExample, expected);
+    [DataTestMethod, Timeout(Timeout)]
+    [DataRow(1, false, "2565", DisplayName = "2015.03.1-problem")]
+    [DataRow(2, false, "2639", DisplayName = "2015.03.2-problem")]
+    public void Solve_2015_03(int part, bool useExample, string expected) =>
+        Solve(new Puzzle03Solver(), 3, part, useExample, expected);
+
+    [DataTestMethod, Timeout(4000)]
+    [DataRow(1, false, "282749", DisplayName = "2015.04.1-problem")]
+    [DataRow(2, false, "9962624", DisplayName = "2015.04.2-problem")]
+    public void Solve_2015_04(int part, bool useExample, string expected) =>
+        Solve(new Puzzle04Solver(), 4, part, useExample, expected);
 }
