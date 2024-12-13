b5c9be15b8da27a4a6d71f4b0fba79a23b6b3692
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-13 17:19:48 -0500

    2024 puzzle 9 part 2 - much faster algorithm

--- CSharp/AdventOfCode.Solvers/2024/Puzzle09Solver.cs
+++ CSharp/AdventOfCode.Solvers/2024/Puzzle09Solver.cs
@@ -53,78 +53,52 @@ public class Puzzle09Solver : IPuzzleSolver
 
     public string SolvePartTwo(string input)
     {
-        var memory = LoadMemory2(input);
-        memory = Defrag2(memory);
-        return Checksum2(memory).ToString();
+        var memory = LoadMemory(input);
+        Defrag(memory);
+        return Checksum(memory).ToString();
     }
 
-    private static List<MemChunk> LoadMemory2(string input) =>
-        input.ToCharArray().Select(c => c - '0')
-        .Select((bs, i) => new MemChunk(i % 2 == 0 ? i / 2 : -1, bs))
-        .Where(c => c.Length > 0)
-        .ToList();
-
-    private static List<MemChunk> Defrag2(List<MemChunk> memory)
+    private static List<MemChunk> LoadMemory(string input)
     {
-        for (var id = memory.Last().Id; id >= 0; id--)
+        var values = input.ReplaceLineEndings("").ToCharArray().Select(c => c - '0');
+        var index = 0;
+        var result = new List<MemChunk>();
+        foreach (var it in values.Select((v, i) => (v, i)))
         {
-            var memIndex = memory.Index();
-            var chunkHere = memIndex.Single(m => m.Item.Id == id);
+            result.Add(new MemChunk(index, it.i % 2 == 0 ? it.i / 2 : -1, it.v));
+            index += it.v;
+        }
+        return result;
+    }
 
-            var firstEmptyChunk = memory.Index()
-                .FirstOrDefault(x =>
-                    x.Item.Id == -1 &&                       // it's empty (no id)
-                    x.Index < chunkHere.Index &&             // it's earlier in memory
-                    x.Item.Length >= chunkHere.Item.Length); // it would fit
-
-            if (firstEmptyChunk.Item is null)
-                continue;
-
-            var leftoverEmptyLength = firstEmptyChunk.Item.Length - chunkHere.Item.Length;
-
-            // Replace old slot with empty memory
-            memory.RemoveAt(chunkHere.Index);
-            memory.Insert(chunkHere.Index, new MemChunk(-1, chunkHere.Item.Length));
-
-            // Replace new slot with filled memory + leftovers
-            memory.RemoveAt(firstEmptyChunk.Index);
-            memory.Insert(firstEmptyChunk.Index, new MemChunk(chunkHere.Item.Id, chunkHere.Item.Length));
-            memory.Insert(firstEmptyChunk.Index + 1, new MemChunk(-1, leftoverEmptyLength));
-
-            // Merge consecutive empty chunks
-            for (var i = 0; i < memory.Count - 1; i++)
+    private static void Defrag(List<MemChunk> memory)
+    {
+        for (var j = memory.Count - 1; j > 1; j -= 2)
+        {
+            for (int i = 1; i < j; i += 2)
             {
-                while (i < memory.Count - 1 && memory[i].Id == -1 && memory[i + 1].Id == -1)
+                var used = memory[j];
+                var empty = memory[i];
+                if (empty.Length >= used.Length)
                 {
-                    var totalLength = memory[i].Length + memory[i + 1].Length;
-                    memory.RemoveAt(i);
-                    memory.RemoveAt(i);
-                    memory.Insert(i, new MemChunk(-1, totalLength));
+                    used.Index = empty.Index;
+                    empty.Index += used.Length;
+                    empty.Length -= used.Length;
+                    break;
                 }
             }
-
-            memory = memory.Where(m => m.Length > 0).ToList();
         }
-
-        return memory;
     }
 
-    private static ulong Checksum2(List<MemChunk> memory)
+    private static ulong Checksum(List<MemChunk> memory) =>
+        memory.Where(m => m.Id > 0).Aggregate(0UL, (aggregate, chunk) =>
+            aggregate + Enumerable.Range(0, chunk.Length)
+                .Aggregate(0UL, (a, b) => a + (ulong)((chunk.Index + b) * chunk.Id)));
+
+    private class MemChunk(int Index, int Id, int Length)
     {
-        var checksum = 0UL;
-
-        var block = 0;
-        for (var i = 0; i < memory.Count; i++)
-        {
-            for (var j = 0; j < memory[i].Length; j++)
-            {
-                checksum += memory[i].Id >= 0 ? (ulong)(block * memory[i].Id) : 0;
-                block++;
-            }
-        }
-
-        return checksum;
+        public int Index { get; set; } = Index;
+        public int Id { get; } = Id;
+        public int Length { get; set; } = Length;
     }
-
-    private record MemChunk(int Id, int Length);
 }
