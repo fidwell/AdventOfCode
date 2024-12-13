c9600f35a1a09d00d2a588593eeb608e637a28fa
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-13 12:10:39 -0500

    Aggregate benchmarks

--- CSharp/AdventOfCode.ConsoleApp/Benchmarker.cs
+++ CSharp/AdventOfCode.ConsoleApp/Benchmarker.cs
@@ -21,9 +21,9 @@ internal static class Benchmarker
             return;
         }
 
-        ConsoleWriter.Write("|-----|------|-------------|");
-        ConsoleWriter.Write("| Day | Part | Time        |");
-        ConsoleWriter.Write("|-----|------|-------------|");
+        ConsoleWriter.Write("|-----|------|---------|-------------|-------------|-------------|");
+        ConsoleWriter.Write("| Day | Part | Samples | Mean time   | Min time    | Max time    |");
+        ConsoleWriter.Write("|-----|------|---------|-------------|-------------|-------------|");
 
         foreach (var solver in solvers)
         {
@@ -34,42 +34,70 @@ internal static class Benchmarker
             var input = DataReader.GetData(year, dayNum, 0, false);
 
             // Maybe run in a background worker
-            WritePartLine(dayNum, 1, SolvePart(() => solver.SolvePartOne(input)));
-            WritePartLine(dayNum, 2, SolvePart(() => solver.SolvePartTwo(input)));
+            WritePartLine(dayNum, 1, AggregateSolves(() => solver.SolvePartOne(input)));
+            WritePartLine(dayNum, 2, AggregateSolves(() => solver.SolvePartTwo(input)));
 
-            ConsoleWriter.Write("|-----|------|-------------|");
+            ConsoleWriter.Write("|-----|------|---------|-------------|-------------|-------------|");
         }
     }
 
-    private static Stopwatch SolvePart(Func<string> action)
+    private static Aggregate AggregateSolves(Func<string> action)
+    {
+        var results = new List<TimeSpan>();
+
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        while (results.Count < 20 && stopwatch.Elapsed.TotalSeconds < 1)
+        {
+            results.Add(SolvePart(action));
+        }
+        stopwatch.Stop();
+
+        return new Aggregate(
+            results.Count,
+            new TimeSpan((long)results.Average(r => r.Ticks)),
+            results.Min(),
+            results.Max());
+    }
+
+    private static TimeSpan SolvePart(Func<string> action)
     {
         var stopwatch = new Stopwatch();
         stopwatch.Start();
         action();
         stopwatch.Stop();
-        return stopwatch;
+        return stopwatch.Elapsed;
     }
 
-    private static void WritePartLine(int day, int part, Stopwatch stopwatch)
+    private static void WritePartLine(int day, int part, Aggregate aggregate)
     {
-        var duration = new TimeSpan(stopwatch.ElapsedTicks);
         var dayStr = day.ToString().PadLeft(2, ' ');
-        Console.Write($"|  {dayStr} |    {part} | ");
+        var samplesStr = aggregate.Count.ToString().PadLeft(7, ' ');
+        Console.Write($"|  {dayStr} |    {part} | {samplesStr} | ");
+        WriteTime(aggregate.Mean);
+        Console.Write(" | ");
+        WriteTime(aggregate.Min);
+        Console.Write(" | ");
+        WriteTime(aggregate.Max);
+        Console.WriteLine(" |");
+    }
+
+    private static void WriteTime(TimeSpan duration)
+    {
         Console.ForegroundColor = TimeColor(duration);
         Console.Write(ElapsedTimeString(duration).PadLeft(11, ' '));
         Console.ForegroundColor = ConsoleColor.Gray;
-        Console.WriteLine(" |");
     }
 
     private static string ElapsedTimeString(TimeSpan duration)
     {
         if (duration.TotalMilliseconds < 1)
-            return $"{duration.TotalMicroseconds:N4} μs";
+            return $"{duration.TotalMicroseconds:N1} μs";
 
         if (duration.TotalSeconds < 1)
-            return $"{duration.TotalMilliseconds:N4} ms";
+            return $"{duration.TotalMilliseconds:N1} ms";
 
-        return $"{duration.TotalSeconds:N4} s ";
+        return $"{duration.TotalSeconds:N1} s ";
     }
 
     private static ConsoleColor TimeColor(TimeSpan duration)
@@ -88,4 +116,6 @@ internal static class Benchmarker
 
         return ConsoleColor.Cyan;
     }
+
+    private record Aggregate(int Count, TimeSpan Mean, TimeSpan Min, TimeSpan Max);
 }
