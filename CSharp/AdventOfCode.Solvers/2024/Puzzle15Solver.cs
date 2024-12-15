a079871a5f564005fda79e38226eaeecf49af5c9
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-15 14:25:55 -0500

    2024 puzzle 15 part 2 - combined solutions

--- CSharp/AdventOfCode.Solvers/2024/Puzzle15Solver.cs
+++ CSharp/AdventOfCode.Solvers/2024/Puzzle15Solver.cs
@@ -5,22 +5,24 @@ namespace AdventOfCode.Solvers._2024;
 
 public class Puzzle15Solver : IPuzzleSolver
 {
-    public string SolvePartOne(string input)
-    {
-        var warehouse = new Warehouse(input);
-        while (!warehouse.IsDone)
-        {
-            warehouse.Step();
-        }
-        return warehouse.BoxLocationSum.ToString();
-    }
+    // Disabled for benchmarking and performance.
+    // Toggle on if you want to see the output!
+    const bool ShouldPrint = false;
 
-    public string SolvePartTwo(string input)
+    public string SolvePartOne(string input) => Solve(input, false);
+    public string SolvePartTwo(string input) => Solve(input, true);
+
+    private static string Solve(string input, bool isPartTwo)
     {
-        var warehouse = new WideWarehouse(input);
+        var warehouse = new Warehouse(input, isPartTwo);
         while (!warehouse.IsDone)
         {
             warehouse.Step();
+
+            if (ShouldPrint)
+            {
+                warehouse.Print();
+            }
         }
         return warehouse.BoxLocationSum.ToString();
     }
@@ -31,14 +33,16 @@ public class Puzzle15Solver : IPuzzleSolver
         public List<(int, int)> Boxes { get; } = [];
         public List<(int, int)> Walls { get; } = [];
         public string RobotInstructions { get; private set; } = string.Empty;
-
+        public bool AreBoxesWide { get; }
         private readonly int _size;
-        private int _step = 0;
 
+        private int _step = 0;
         public bool IsDone => _step >= RobotInstructions.Length;
 
-        public Warehouse(string input)
+        public Warehouse(string input, bool isWide)
         {
+            AreBoxesWide = isWide;
+
             var y = 0;
             foreach (var line in input.SplitByNewline())
             {
@@ -47,11 +51,21 @@ public class Puzzle15Solver : IPuzzleSolver
                     for (var x = 0; x < line.Length; x++)
                     {
                         if (line[x] == '@')
-                            Robot = (x, y);
+                            Robot = (AreBoxesWide ? x * 2 : x, y);
                         else if (line[x] == 'O')
-                            Boxes.Add((x, y));
+                            Boxes.Add((AreBoxesWide ? x * 2 : x, y));
                         else if (line[x] == '#')
-                            Walls.Add((x, y));
+                        {
+                            if (AreBoxesWide)
+                            {
+                                Walls.Add((x * 2, y));
+                                Walls.Add((x * 2 + 1, y));
+                            }
+                            else
+                            {
+                                Walls.Add((x, y));
+                            }
+                        }
                     }
 
                     y++;
@@ -68,113 +82,14 @@ public class Puzzle15Solver : IPuzzleSolver
         {
             var direction = RobotInstructions[_step].ToDirection();
             var robotTarget = Robot.Go(direction);
-            if (PushThingAt(robotTarget, direction))
-            {
-                Robot = robotTarget;
-            }
-            _step++;
-        }
-
-        private bool PushThingAt((int, int) location, Direction direction)
-        {
-            if (Walls.Contains(location))
-                return false;
-
-            if (Boxes.Contains(location))
-            {
-                var target = location.Go(direction);
-                if (PushThingAt(target, direction))
-                {
-                    Boxes.RemoveAll(b => b == location);
-                    Boxes.Add(target);
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        public int BoxLocationSum => Boxes.Sum(b => b.Item1 + 100 * b.Item2);
-
-        public void Print()
-        {
-            if (_step > 0)
-                Console.WriteLine(RobotInstructions[_step - 1]);
-            for (int y = 0; y < _size; y++)
-            {
-                for (int x = 0; x < _size; x++)
-                {
-                    if (Walls.Contains((x, y)))
-                        Console.Write('#');
-                    else if (Boxes.Contains((x, y)))
-                        Console.Write('O');
-                    else if (Robot == (x, y))
-                        Console.Write('@');
-                    else
-                        Console.Write('.');
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-        }
-    }
-
-    private class WideWarehouse
-    {
-        public (int, int) Robot { get; private set; }
-        public List<(int, int)> Boxes { get; } = [];
-        public List<(int, int)> Walls { get; } = [];
-        public string RobotInstructions { get; private set; } = string.Empty;
-
-        private int _step = 0;
-
-        public bool IsDone => _step >= RobotInstructions.Length;
-
-        public WideWarehouse(string input)
-        {
-            var y = 0;
-            foreach (var line in input.SplitByNewline())
-            {
-                if (line.StartsWith('#'))
-                {
-                    for (var x = 0; x < line.Length; x++)
-                    {
-                        if (line[x] == '@')
-                            Robot = (x * 2, y);
-                        else if (line[x] == 'O')
-                            Boxes.Add((x * 2, y));
-                        else if (line[x] == '#')
-                        {
-                            Walls.Add((x * 2, y));
-                            Walls.Add((x * 2 + 1, y));
-                        }
-                    }
-
-                    y++;
-                }
-                else
-                {
-                    RobotInstructions = $"{RobotInstructions}{line}";
-                }
-            }
-        }
-
-        public void Step()
-        {
-            var direction = RobotInstructions[_step].ToDirection();
-            var robotTarget = Robot.Go(direction);
-            var boxTree = CreateDependencyTree(Boxes, robotTarget, direction);
+            var boxTree = CreateDependencyTree(robotTarget, direction);
 
             if (boxTree is null && !Walls.Any(w => w == robotTarget))
             {
                 // no boxes; move at will
                 Robot = robotTarget;
             }
-            else if (boxTree?.CanMove(Walls, direction) ?? false)
+            else if (boxTree?.CanMove(Walls, direction, AreBoxesWide) ?? false)
             {
                 boxTree.Move(Boxes, direction);
                 Robot = robotTarget;
@@ -183,18 +98,18 @@ public class Puzzle15Solver : IPuzzleSolver
             _step++;
         }
 
-        private BoxDependency? CreateDependencyTree(List<(int, int)> boxes, (int, int) target, Direction direction)
+        private BoxDependency? CreateDependencyTree((int, int) target, Direction direction)
         {
-            var firstBox = Boxes.Where(b => b == target || b == target.Go(Direction.Left));
+            var firstBox = Boxes.Where(b => b == target || (AreBoxesWide && b == target.Go(Direction.Left)));
             if (!firstBox.Any())
             {
                 return null;
             }
 
-            return IterateDependency(firstBox.Single(), boxes, direction);
+            return IterateDependency(firstBox.Single(), direction);
         }
 
-        private static BoxDependency IterateDependency((int, int) box, List<(int, int)> boxes, Direction direction)
+        private BoxDependency IterateDependency((int, int) box, Direction direction)
         {
             IEnumerable<(int, int)> subBoxes;
             switch (direction)
@@ -204,32 +119,46 @@ public class Puzzle15Solver : IPuzzleSolver
                  * [][].
                  */
                 case Direction.Down:
-                    var dl = box.Go(Direction.Down).Go(Direction.Left);
-                    var dm = box.Go(Direction.Down);
-                    var dr = box.Go(Direction.Down).Go(Direction.Right);
-                    subBoxes = new List<(int, int)> { dl, dm, dr }.Where(boxes.Contains);
+                    if (AreBoxesWide)
+                    {
+                        var dl = box.Go(Direction.Down).Go(Direction.Left);
+                        var dm = box.Go(Direction.Down);
+                        var dr = box.Go(Direction.Down).Go(Direction.Right);
+                        subBoxes = new List<(int, int)> { dl, dm, dr }.Where(Boxes.Contains);
+                    }
+                    else
+                    {
+                        subBoxes = Boxes.Where(b => b == box.Go(Direction.Down));
+                    }
                     break;
 
                 case Direction.Up:
-                    var ul = box.Go(Direction.Up).Go(Direction.Left);
-                    var um = box.Go(Direction.Up);
-                    var ur = box.Go(Direction.Up).Go(Direction.Right);
-                    subBoxes = new List<(int, int)> { ul, um, ur }.Where(boxes.Contains);
+                    if (AreBoxesWide)
+                    {
+                        var ul = box.Go(Direction.Up).Go(Direction.Left);
+                        var um = box.Go(Direction.Up);
+                        var ur = box.Go(Direction.Up).Go(Direction.Right);
+                        subBoxes = new List<(int, int)> { ul, um, ur }.Where(Boxes.Contains);
+                    }
+                    else
+                    {
+                        subBoxes = Boxes.Where(b => b == box.Go(Direction.Up));
+                    }
                     break;
 
                 // >[][]
                 case Direction.Right:
-                    subBoxes = boxes.Where(b => b == box.Go(Direction.Right, 2));
+                    subBoxes = Boxes.Where(b => b == box.Go(Direction.Right, AreBoxesWide ? 2 : 1));
                     break;
 
                 // [][]<
                 case Direction.Left:
-                    subBoxes = boxes.Where(b => b == box.Go(Direction.Left, 2));
+                    subBoxes = Boxes.Where(b => b == box.Go(Direction.Left, AreBoxesWide ? 2 : 1));
                     break;
 
                 default: throw new ArgumentException(nameof(direction));
             }
-            return new BoxDependency(box, subBoxes.Select(b => IterateDependency(b, boxes, direction)));
+            return new BoxDependency(box, subBoxes.Select(b => IterateDependency(b, direction)));
         }
 
         public int BoxLocationSum => Boxes.Sum(b => b.Item1 + 100 * b.Item2);
@@ -239,13 +168,14 @@ public class Puzzle15Solver : IPuzzleSolver
             public (int, int) Root { get; } = root;
             public List<BoxDependency> Branches { get; } = branches.ToList();
 
-            public bool CanMove(List<(int, int)> walls, Direction direction)
+            public bool CanMove(List<(int, int)> walls, Direction direction, bool areBoxesWide)
             {
                 if (walls.Any(w =>
                     w == Root.Go(direction) ||
-                    w == Root.Go(Direction.Right).Go(direction)))
+                    (areBoxesWide && w == Root.Go(Direction.Right).Go(direction))))
                     return false;
-                return Branches.All(b => b.CanMove(walls, direction));
+
+                return Branches.All(b => b.CanMove(walls, direction, areBoxesWide));
             }
 
             public void Move(List<(int, int)> boxes, Direction direction)
@@ -263,6 +193,39 @@ public class Puzzle15Solver : IPuzzleSolver
                     boxes.Add(Root.Go(direction));
                 }
             }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_step);
+            if (_step > 0)
+                Console.WriteLine(RobotInstructions[_step - 1]);
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size * (AreBoxesWide ? 2 : 1); x++)
+                {
+                    if (Walls.Contains((x, y)))
+                        Console.Write('#');
+                    else if (Boxes.Contains((x, y)))
+                    {
+                        if (AreBoxesWide)
+                        {
+                            Console.Write("[]");
+                            x++;
+                        }
+                        else
+                        {
+                            Console.Write('O');
+                        }
+                    }
+                    else if (Robot == (x, y))
+                        Console.Write('@');
+                    else
+                        Console.Write('.');
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
