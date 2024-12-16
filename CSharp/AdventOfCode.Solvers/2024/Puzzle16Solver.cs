685f296034e585a774517143d38c1072e9b88190
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-16 17:32:26 -0500

    2024 puzzle 16 part 1 - refactored and working

--- CSharp/AdventOfCode.Solvers/2024/Puzzle16Solver.cs
+++ CSharp/AdventOfCode.Solvers/2024/Puzzle16Solver.cs
@@ -1,37 +1,49 @@
 ﻿using System.Collections.Immutable;
 using AdventOfCode.Core.ArrayUtilities;
 using AdventOfCode.Core.StringUtilities;
-using Coord = (int, int);
 
 namespace AdventOfCode.Solvers._2024;
 
-using Path = ImmutableStack<Coord>;
-
-// adapted from https://github.com/tmbarker/advent-of-code/blob/main/Solutions/Y2024/D16/Solution.cs
-// much work to do to make it my own / more reusable
 public class Puzzle16Solver : IPuzzleSolver
 {
-    public string SolvePartOne(string input) => Solve(input).Item1.ToString();
+    // Disabled for benchmarking and performance.
+    // Toggle on if you want to see the output!
+    const bool ShouldPrint = false;
+
+    public string SolvePartOne(string input)
+    {
+        var matrix = new CharacterMatrix(input);
+        var result = Solve(matrix);
+        if (ShouldPrint)
+        {
+            Print(matrix, result.Item2.First().Select(p => p));
+        }
+        return result.Item1.ToString();
+    }
 
     public string SolvePartTwo(string input)
     {
-        var results = Solve(input);
+        var matrix = new CharacterMatrix(input);
+        var results = Solve(matrix);
         var coords = results.Item2.SelectMany(p => p).Distinct().ToList();
+        if (ShouldPrint)
+        {
+            Print(matrix, coords);
+        }
         return (coords.Count + 1).ToString();
     }
 
-    private static (int, List<Path>) Solve(string input)
+    private static (int, List<List<(int, int)>>) Solve(CharacterMatrix matrix)
     {
-        var matrix = new CharacterMatrix(input);
         var start = (1, matrix.Height - 2);
         var end = (matrix.Width - 2, 1);
 
         var bestScore = int.MaxValue;
-        var bestPaths = new List<Path>();
+        var bestPaths = new List<List<(int, int)>>();
         var minScores = new Dictionary<ReindeerState, int>();
 
-        var queue = new Queue<State>();
-        queue.Enqueue(new State(new ReindeerState(start, Direction.Right), 0, []));
+        var queue = new PriorityQueue<State, int>();
+        queue.Enqueue(new State(new ReindeerState(start, Direction.Right), 0, []), 0);
 
         while (queue.Count > 0)
         {
@@ -61,8 +73,6 @@ public class Puzzle16Solver : IPuzzleSolver
                 Enqueue(state.TurnRight());
         }
 
-        Print(matrix, bestPaths[0]);
-
         return (bestScore, bestPaths);
 
         void Enqueue(State state)
@@ -71,15 +81,15 @@ public class Puzzle16Solver : IPuzzleSolver
             {
                 value = state.Score;
                 minScores[state.ReindeerState] = value;
-                queue.Enqueue(state);
+                queue.Enqueue(state, value);
             }
         }
     }
 
-    private readonly record struct State(ReindeerState ReindeerState, int Score, Path Path)
+    private readonly record struct State(ReindeerState ReindeerState, int Score, List<(int, int)> Path)
     {
         public readonly State Forward() =>
-            new(ReindeerState.Forward(), Score + 1, Path.Push(ReindeerState.Location.Go(ReindeerState.Direction)));
+            new(ReindeerState.Forward(), Score + 1, [.. Path, ReindeerState.Location.Go(ReindeerState.Direction)]);
 
         public readonly State TurnLeft() =>
             new(ReindeerState.TurnLeft(), Score + 1000, Path);
@@ -88,14 +98,14 @@ public class Puzzle16Solver : IPuzzleSolver
             new(ReindeerState.TurnRight(), Score + 1000, Path);
     }
 
-    private record struct ReindeerState(Coord Location, Direction Direction)
+    private record struct ReindeerState((int, int) Location, Direction Direction)
     {
         public readonly ReindeerState Forward() => new(Location.Go(Direction), Direction);
         public readonly ReindeerState TurnLeft() => new(Location, Direction.RotateLeft());
         public readonly ReindeerState TurnRight() => new(Location, Direction.RotateRight());
     }
 
-    private static void Print(CharacterMatrix matrix, IEnumerable<Coord> coords)
+    private static void Print(CharacterMatrix matrix, IEnumerable<(int, int)> coords)
     {
         for (var y = 0; y < matrix.Height; y++)
         {
