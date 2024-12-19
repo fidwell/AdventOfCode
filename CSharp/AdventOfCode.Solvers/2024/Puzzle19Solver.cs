63ed61304ed92a8837b2a1fc4a54d72a6f1c152d
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-19 08:50:41 -0500

    2024 puzzle 19 - cleanup

--- CSharp/AdventOfCode.Solvers/2024/Puzzle19Solver.cs
+++ CSharp/AdventOfCode.Solvers/2024/Puzzle19Solver.cs
@@ -6,68 +6,52 @@ public class Puzzle19Solver : PuzzleSolver
 {
     public override string SolvePartOne(string input)
     {
-        var lines = input.SplitByNewline();
-        var atoms = lines[0].Split(", ");
-        var targets = lines.Skip(1);
+        var (atoms, targets) = Parse(input);
         var cache = Preprocess(atoms);
         return targets.Count(t => SolutionCount(t, atoms, cache) > 0).ToString();
     }
 
     public override string SolvePartTwo(string input)
     {
-        var lines = input.SplitByNewline();
-        var atoms = lines[0].Split(", ").OrderBy(a => a.Length);
-        var targets = lines.Skip(1);
+        var (atoms, targets) = Parse(input);
         var cache = Preprocess(atoms);
         return targets.Aggregate(0UL, (a, b) => a + SolutionCount(b, atoms, cache)).ToString();
+    }
+
+    private static (IEnumerable<string>, IEnumerable<string>) Parse(string input)
+    {
+        var lines = input.SplitByNewline();
+        return (lines[0].Split(", "), lines.Skip(1));
     }
 
     private static Dictionary<string, ulong> Preprocess(IEnumerable<string> atoms)
     {
         var cache = new Dictionary<string, ulong>();
-        foreach (var atom in atoms)
+        foreach (var atom in atoms.OrderBy(a => a.Length))
         {
             cache[atom] = 1 + SolutionCount(atom, atoms, cache, isPreprocessing: true);
         }
         return cache;
     }
 
-    private static ulong SolutionCount(string target, IEnumerable<string> atoms, Dictionary<string, ulong> cache, bool isPreprocessing = false)
+    private static ulong SolutionCount(
+        string target,
+        IEnumerable<string> atoms,
+        Dictionary<string, ulong> cache,
+        bool isPreprocessing = false)
     {
         if (!isPreprocessing && cache.TryGetValue(target, out ulong cachedValue))
-        {
             return cachedValue;
+
+        foreach (var atom in atoms.Where(target.StartsWith))
+        {
+            var substring = target[atom.Length..];
+            var count = SolutionCount(substring, atoms, cache);
+            _ = cache.TryGetValue(target, out ulong existing);
+            cache[target] = existing + count;
         }
 
-        foreach (var atom in atoms)
-        {
-            if (target.StartsWith(atom))
-            {
-                var substring = target[atom.Length..];
-                var count = SolutionCount(substring, atoms, cache);
-
-                if (cache.TryGetValue(target, out ulong existing))
-                {
-                    cache[target] = existing + count;
-                }
-                else
-                {
-                    Console.WriteLine($"{atom}|{substring}: {count}");
-                    cache[target] = count;
-                }
-            }
-        }
-        
-        if (cache.TryGetValue(target, out ulong value))
-        {
-            Console.WriteLine($"{cache[target]} ways to make {target}");
-            return value;
-        }
-        else
-        {
-            Console.WriteLine($"Can't make {target}");
-            cache[target] = 0;
-            return 0;
-        }
+        _ = cache.TryGetValue(target, out ulong value);
+        return value;
     }
 }
