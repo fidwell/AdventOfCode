06c07e4601d5e9f73a42d8ede9bf905d98f48784
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-20 02:13:31 -0500

    2024 puzzle 20 part 2

--- CSharp/AdventOfCode.Solvers/2024/Puzzle20Solver.cs
+++ CSharp/AdventOfCode.Solvers/2024/Puzzle20Solver.cs
@@ -1,4 +1,5 @@
-﻿using AdventOfCode.Core.StringUtilities;
+﻿using AdventOfCode.Core.MathUtilities;
+using AdventOfCode.Core.StringUtilities;
 
 namespace AdventOfCode.Solvers._2024;
 
@@ -7,7 +8,7 @@ public class Puzzle20Solver : PuzzleSolver
     public override string SolvePartOne(string input)
     {
         var maze = new CharacterMatrix(input);
-        var solution = Solve(maze, (-1, -1));
+        var solution = Solve(maze);
         var end = maze.SingleMatch('E');
         var totalSteps = solution[end.Item1, end.Item2];
         Console.WriteLine($"Without cheating, solved in {totalSteps} steps");
@@ -45,10 +46,72 @@ public class Puzzle20Solver : PuzzleSolver
 
     public override string SolvePartTwo(string input)
     {
-        throw new NotImplementedException();
+        var maze = new CharacterMatrix(input);
+        var solution = Solve(maze);
+        var end = maze.SingleMatch('E');
+        var totalSteps = solution[end.Item1, end.Item2];
+        Console.WriteLine($"Without cheating, solved in {totalSteps} steps");
+        var isExample = maze.Width == 15;
+
+        const int cheatMax = 20;
+        var cheatsThatWouldSaveAtLeast100Steps = 0;
+        var savingsDict = new Dictionary<int, int>();
+
+        for (var y = 1; y < maze.Height - 1; y++)
+        {
+            for (var x = 1; x < maze.Width - 1; x++)
+            {
+                if (maze.CharAt((x, y)) == '#')
+                    continue;
+
+                var cheatStart = (x, y);
+                // Find cheat endpoint
+                for (var y0 = y - cheatMax - 5; y0 < y + cheatMax + 5; y0++)
+                {
+                    for (var x0 = x - cheatMax - 5; x0 < x + cheatMax + 5; x0++)
+                    {
+                        if (x0 < 0 || y0 < 0 || x0 >= maze.Width - 1 || y0 >= maze.Height - 1)
+                            continue;
+
+                        if (maze.CharAt((x0, y0)) == '#')
+                            continue;
+
+                        var cheatEnd = (x0, y0);
+                        var manhattanDistance = MathExtensions.ManhattanDistance(cheatStart, cheatEnd);
+                        if (manhattanDistance > cheatMax)
+                            continue;
+
+                        var costAtStart = solution[cheatStart.x, cheatStart.y];
+                        var costAtEnd = solution[cheatEnd.x0, cheatEnd.y0];
+                        var difference = costAtEnd - costAtStart;
+                        var totalGain = difference - manhattanDistance;
+
+                        if (totalGain >= (isExample ? 50 : 100))
+                        {
+                            if (savingsDict.ContainsKey(totalGain))
+                            {
+                                savingsDict[totalGain]++;
+                            }
+                            else
+                            {
+                                savingsDict[totalGain] = 1;
+                            }
+                            cheatsThatWouldSaveAtLeast100Steps++;
+                        }
+                    }
+                }
+            }
+        }
+
+        foreach (var entry in savingsDict.OrderBy(d => d.Key))
+        {
+            Console.WriteLine($"There are {entry.Value} cheats that save {entry.Key} picoseconds.");
+        }
+
+        return cheatsThatWouldSaveAtLeast100Steps.ToString();
     }
 
-    private static int[,] Solve(CharacterMatrix maze, (int, int) cheat)
+    private static int[,] Solve(CharacterMatrix maze)
     {
         var start = maze.SingleMatch('S');
         var end = maze.SingleMatch('E');
@@ -78,7 +141,7 @@ public class Puzzle20Solver : PuzzleSolver
             IEnumerable<RaceState> possibleNextStates = [];
 
             possibleNextStates = possibleNextStates.Concat(neighbors
-                .Where(n => maze.CharAt(n) != '#' || n == cheat)
+                .Where(n => maze.CharAt(n) != '#')
                 .Select(n => new RaceState(n, state.PathLength + 1)));
 
             foreach (var nextState in possibleNextStates)
