c17b76558b49304c6ee6268a0112730debbd2f40
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-20 12:52:35 -0500

    2024 puzzle 20 - genericize solving algorithm

--- CSharp/AdventOfCode.Solvers/2024/Puzzle20Solver.cs
+++ CSharp/AdventOfCode.Solvers/2024/Puzzle20Solver.cs
@@ -8,57 +8,32 @@ public class Puzzle20Solver : PuzzleSolver
     public override string SolvePartOne(string input)
     {
         var maze = new CharacterMatrix(input);
-        var solution = CalculateStepCosts(maze);
-        var end = maze.SingleMatch('E');
-        var totalSteps = solution[end.Item1, end.Item2];
-        //Console.WriteLine($"Without cheating, solved in {totalSteps} steps");
         var isExample = maze.Width == 15;
-
-        var cheatsThatWouldSaveAtLeast100Steps = 0;
-
-        for (var y = 1; y < maze.Height - 1; y++)
-        {
-            for (var x = 1; x < maze.Width - 1; x++)
-            {
-                if (maze.CharAt((x, y)) != '#')
-                    continue;
-
-                var neighbors = maze.CoordinatesOfNeighbors((x, y), allEight: false)
-                    .Where(n => solution[n.Item1, n.Item2] > 0)
-                    .ToArray();
-                if (neighbors.Length == 2)
-                {
-                    var n1 = neighbors[0];
-                    var n2 = neighbors[1];
-                    var n1am = solution[n1.Item1, n1.Item2];
-                    var n2am = solution[n2.Item1, n2.Item2];
-                    var difference = Math.Abs(n1am - n2am) - 2;
-                    if (difference >= (isExample ? 1 : 100))
-                    {
-                        if (isExample)
-                            Console.WriteLine($"With cheat at {(x, y)}, saved {difference} steps");
-                        cheatsThatWouldSaveAtLeast100Steps++;
-                    }
-                }
-            }
-        }
-
-        return cheatsThatWouldSaveAtLeast100Steps.ToString();
+        return Solve(maze, isExample ? 1 : 100, 2);
     }
 
     public override string SolvePartTwo(string input)
     {
         var maze = new CharacterMatrix(input);
-        var solution = CalculateStepCosts(maze);
-        var end = maze.SingleMatch('E');
-        var totalSteps = solution[end.Item1, end.Item2];
-        Console.WriteLine($"Without cheating, solved in {totalSteps} steps");
         var isExample = maze.Width == 15;
+        return Solve(maze, isExample ? 50 : 100, 20);
+    }
 
-        const int cheatMax = 20;
-        var cheatsThatWouldSaveAtLeast100Steps = 0;
+    public string Solve(CharacterMatrix maze, int targetSavings, int cheatMax)
+    {
+        var stepCosts = CalculateStepCosts(maze);
+        var end = maze.SingleMatch('E');
+        var totalSteps = stepCosts[end.Item1, end.Item2];
+
+        if (ShouldPrint)
+        {
+            Console.WriteLine($"Without cheating, solved in {totalSteps} steps");
+        }
+
+        var cheatsThatWouldSaveAtLeastXSteps = 0;
         var savingsDict = new Dictionary<int, int>();
 
+        // Find cheat start point
         for (var y = 1; y < maze.Height - 1; y++)
         {
             for (var x = 1; x < maze.Width - 1; x++)
@@ -67,7 +42,8 @@ public class Puzzle20Solver : PuzzleSolver
                     continue;
 
                 var cheatStart = (x, y);
-                // Find cheat endpoint
+
+                // Find cheat end point
                 for (var y0 = y - cheatMax - 5; y0 < y + cheatMax + 5; y0++)
                 {
                     for (var x0 = x - cheatMax - 5; x0 < x + cheatMax + 5; x0++)
@@ -83,12 +59,12 @@ public class Puzzle20Solver : PuzzleSolver
                         if (manhattanDistance > cheatMax)
                             continue;
 
-                        var costAtStart = solution[cheatStart.x, cheatStart.y];
-                        var costAtEnd = solution[cheatEnd.x0, cheatEnd.y0];
+                        var costAtStart = stepCosts[cheatStart.x, cheatStart.y];
+                        var costAtEnd = stepCosts[cheatEnd.x0, cheatEnd.y0];
                         var difference = costAtEnd - costAtStart;
                         var totalGain = difference - manhattanDistance;
 
-                        if (totalGain >= (isExample ? 50 : 100))
+                        if (totalGain >= targetSavings)
                         {
                             if (savingsDict.ContainsKey(totalGain))
                             {
@@ -98,7 +74,7 @@ public class Puzzle20Solver : PuzzleSolver
                             {
                                 savingsDict[totalGain] = 1;
                             }
-                            cheatsThatWouldSaveAtLeast100Steps++;
+                            cheatsThatWouldSaveAtLeastXSteps++;
                         }
                     }
                 }
@@ -110,7 +86,7 @@ public class Puzzle20Solver : PuzzleSolver
             Console.WriteLine($"There are {entry.Value} cheats that save {entry.Key} picoseconds.");
         }
 
-        return cheatsThatWouldSaveAtLeast100Steps.ToString();
+        return cheatsThatWouldSaveAtLeastXSteps.ToString();
     }
 
     private static int[,] CalculateStepCosts(CharacterMatrix maze)
@@ -129,25 +105,5 @@ public class Puzzle20Solver : PuzzleSolver
             current = next;
         }
         return result;
-    }
-
-    private static void Print(CharacterMatrix maze, IEnumerable<(int, int)> path)
-    {
-        Console.WriteLine();
-        for (var y = 0; y < maze.Height; y++)
-        {
-            for (var x = 0; x < maze.Width; x++)
-            {
-                if (path.Contains((x, y)))
-                {
-                    Console.Write('O');
-                }
-                else
-                {
-                    Console.Write(maze.CharAt(x, y));
-                }
-            }
-            Console.WriteLine();
-        }
     }
 }
