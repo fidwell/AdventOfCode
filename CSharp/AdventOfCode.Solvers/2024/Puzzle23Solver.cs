5fb470d91125abaac98d2389f91fc91771b93531
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-23 10:51:06 -0500

    2024 puzzle 23 - cleanup

--- CSharp/AdventOfCode.Solvers/2024/Puzzle23Solver.cs
+++ CSharp/AdventOfCode.Solvers/2024/Puzzle23Solver.cs
@@ -6,10 +6,9 @@ public class Puzzle23Solver : PuzzleSolver
 {
     public override string SolvePartOne(string input)
     {
-        var allConnections = input.SplitByNewline().Select(l => (l.Substring(0, 2), l.Substring(3, 2)));
-        var groups = GroupsOfFullyConnectedComputers(allConnections, 3);
-        var tGroups = groups.Where(g => g.Any(n => n.StartsWith('t')));
-        return tGroups.Count().ToString();
+        return GroupsOfFullyConnectedComputers(input.SplitByNewline().Select(l => (l.Substring(0, 2), l.Substring(3, 2))))
+            .Where(g => g.Any(n => n.StartsWith('t')))
+            .Count().ToString();
     }
 
     public override string SolvePartTwo(string input)
@@ -65,11 +64,10 @@ public class Puzzle23Solver : PuzzleSolver
         }
     }
 
-    private static List<List<string>> GroupsOfFullyConnectedComputers(IEnumerable<(string, string)> allConnections, int maxSize)
+    private static List<List<string>> GroupsOfFullyConnectedComputers(IEnumerable<(string, string)> allConnections)
     {
         var groupsOfThree = new List<List<string>>();
 
-        // Step 1: Find groups of size 3.
         foreach (var connection1 in allConnections)
         {
             // For each connection in the bag:
@@ -94,61 +92,13 @@ public class Puzzle23Solver : PuzzleSolver
                     var group = new[] { nodeA, nodeB, nodeC }.OrderBy(n => n).ToList();
                     if (!groupsOfThree.Any(g => g.SequenceEqual(group)))
                     {
-                        //Console.WriteLine($"Found a group of 3: {ToString(group)}");
                         groupsOfThree.Add(group);
                     }
                 }
             }
         }
 
-        if (maxSize == 3)
-            return groupsOfThree;
-
-        // Step 2: Find groups of size 4. (To generalize)
-        var groupsOfFour = new List<List<string>>();
-        foreach (var groupOfThree in groupsOfThree)
-        {
-            // For each group of three:
-            // For each node n0:
-            // Find a connection in the bag that has n0, but isn't in the group.
-            // Its other end is n1.
-            // See if connections in the bag exist between n1 and all other nodes in the group.
-            // If so, add it to the result.
-            foreach (var n0 in groupOfThree)
-            {
-                var potentialConnections = allConnections.Where(c =>
-                    (c.Item1 == n0 && !groupOfThree.Contains(c.Item2)) ||
-                    (c.Item2 == n0 && !groupOfThree.Contains(c.Item1)));
-                foreach (var potentialConnection in potentialConnections)
-                {
-                    var n1 = potentialConnection.Item1 == n0 ? potentialConnection.Item2 : potentialConnection.Item1;
-                    var otherNodesInGroup = groupOfThree.Where(n => n != n0);
-                    var groupExists = true;
-                    foreach (var otherNode in otherNodesInGroup)
-                    {
-                        if (!allConnections.Any(c =>
-                            c.Item1 == n1 && c.Item2 == otherNode ||
-                            c.Item2 == n1 && c.Item1 == otherNode))
-                        {
-                            groupExists = false;
-                            break;
-                        }
-                    }
-
-                    if (groupExists)
-                    {
-                        var newGroup = groupOfThree.Concat([n1]).OrderBy(n => n).ToList();
-                        if (!groupsOfFour.Any(g => g.SequenceEqual(newGroup)))
-                        {
-                            Console.WriteLine($"Found a group of 4: {ToString(newGroup)}");
-                            groupsOfFour.Add(newGroup);
-                        }
-                    }
-                }
-            }
-        }
-
-        return groupsOfFour;
+        return groupsOfThree;
     }
 
     private static string ToString(IEnumerable<string> group) => string.Join(',', group);
