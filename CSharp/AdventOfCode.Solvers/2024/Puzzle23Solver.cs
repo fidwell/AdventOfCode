0e8d76fc30b3523a8377b60f21f599e6a243d537
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-23 08:45:10 -0500

    2024 puzzle 23 part 1 - different algorithm

--- CSharp/AdventOfCode.Solvers/2024/Puzzle23Solver.cs
+++ CSharp/AdventOfCode.Solvers/2024/Puzzle23Solver.cs
@@ -7,38 +7,11 @@ public class Puzzle23Solver : PuzzleSolver
     public override string SolvePartOne(string input)
     {
         var allConnections = input.SplitByNewline().Select(l => (l.Substring(0, 2), l.Substring(3, 2)));
-        var uniqueComputerNames = UniqueComputersInConnectionList(allConnections);
 
-        var groups = new HashSet<(string, string, string)>();
-
-        foreach (var computer1 in uniqueComputerNames)
-        {
-            var thisComputerConnections = allConnections.Where(c => c.Item1 == computer1 || c.Item2 == computer1);
-            var uniqueComputersConnectedToComputer1 = UniqueComputersInConnectionList(thisComputerConnections).Where(c => c != computer1);
-            foreach (var computer2 in uniqueComputersConnectedToComputer1)
-            {
-                var secondComputerConnections = allConnections.Where(c => c.Item1 == computer2 || c.Item2 == computer2);
-                var uniqueComputersConnectedToComputer2 = UniqueComputersInConnectionList(secondComputerConnections).Where(c => c != computer2);
-
-                foreach (var computer3 in uniqueComputersConnectedToComputer2)
-                {
-                    if (allConnections.Any(c =>
-                        c.Item1 == computer1 && c.Item2 == computer3 ||
-                        c.Item1 == computer3 && c.Item2 == computer1))
-                    {
-                        if (computer1.StartsWith('t') ||
-                            computer2.StartsWith('t') ||
-                            computer3.StartsWith('t'))
-                        {
-                            var comps = new string[] { computer1, computer2, computer3 }.OrderBy(c => c).ToArray();
-                            groups.Add((comps[0], comps[1], comps[2]));
-                        }
-                    }
-                }
-            }
-        }
-
-        return groups.Count.ToString();
+        var groups = GroupsOfFullyConnectedComputers(allConnections);
+        Console.WriteLine($"Found {groups.Count()} total groups");
+        var tGroups = groups.Where(g => g.Any(n => n.StartsWith('t')));
+        return tGroups.Count().ToString();
     }
 
     public override string SolvePartTwo(string input)
@@ -46,6 +19,44 @@ public class Puzzle23Solver : PuzzleSolver
         throw new NotImplementedException();
     }
 
-    private static IEnumerable<string> UniqueComputersInConnectionList(IEnumerable<(string, string)> connections) =>
-        connections.Select(c => c.Item1).Concat(connections.Select(c => c.Item2)).Distinct();
+    private static List<List<string>> GroupsOfFullyConnectedComputers(IEnumerable<(string, string)> allConnections)
+    {
+        var result = new List<List<string>>();
+
+        // Step 1: Find groups of size 3.
+        foreach (var connection1 in allConnections)
+        {
+            // For each connection in the bag:
+            // Find all second connections with a shared node.
+            // For each of the second connections:
+            // See if there's a third connection that matches.
+            // If so, add it to the result.
+
+            var nodeA = connection1.Item1;
+            var nodeC = connection1.Item2;
+
+            var connection2s = allConnections.Where(c =>
+                c != connection1 &&
+                (c.Item1 == nodeA || c.Item2 == nodeA));
+
+            foreach (var connection2 in connection2s)
+            {
+                var nodeB = connection2.Item1 == nodeA ? connection2.Item2 : connection2.Item1;
+                var matches = allConnections.Where(c => c == (nodeC, nodeB) || c == (nodeB, nodeC));
+                if (matches.Any())
+                {
+                    var group = new[] { nodeA, nodeB, nodeC }.OrderBy(n => n).ToList();
+                    if (!result.Any(g => g.SequenceEqual(group)))
+                    {
+                        Console.WriteLine($"Found a group of 3: {ToString(group)}");
+                        result.Add(group);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static string ToString(IEnumerable<string> group) => $"({string.Join(',', group)})";
 }
