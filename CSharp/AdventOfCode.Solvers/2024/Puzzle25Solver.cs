a4dfb2e551b8bc09eb7eb4fa369b6fb280b27afe
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-25 01:04:36 -0500

    2024 puzzle 25 - simpler solution

--- CSharp/AdventOfCode.Solvers/2024/Puzzle25Solver.cs
+++ CSharp/AdventOfCode.Solvers/2024/Puzzle25Solver.cs
@@ -7,47 +7,24 @@ public class Puzzle25Solver : PuzzleSolver
 {
     public override string SolvePartOne(string input)
     {
-        var chunks = input.SplitByNewline(StringSplitOptions.None).Chunk();
+        var chunks = input.SplitByNewline(StringSplitOptions.None)
+            .Chunk()
+            .Where(c => c.Length > 0)
+            .Select(l => string.Join(string.Empty, l))
+            .ToArray();
+        var count = 0;
 
-        var keys = new List<Key>();
-        var locks = new List<Lock>();
-
-        foreach (var chunk in chunks.Where(c => c.Length > 0))
+        for (var i = 0; i < chunks.Length - 1; i++)
         {
-            if (chunk[0].All(c => c == '#'))
+            for (var j = i + 1; j < chunks.Length; j++)
             {
-                locks.Add(new Lock(Enumerable.Range(0, 5).Select(col =>
-                {
-                    return chunk.Select(c => c[col]).Count(c => c == '#') - 1;
-                }).ToArray()));
-            }
-            else
-            {
-                keys.Add(new Key(Enumerable.Range(0, 5).Select(col =>
-                {
-                    return chunk.Select(c => c[col]).Count(c => c == '#') - 1;
-                }).ToArray()));
+                if (chunks[i].Select((c, i) => (c, i)).All(t => t.c == '.' || chunks[j][t.i] != '#'))
+                    count++;
             }
         }
 
-        var sum = 0;
-        for (var k = 0; k < keys.Count; k++)
-        {
-            for (var l = 0; l < locks.Count; l++)
-            {
-                if (DoFit(keys[k], locks[l]))
-                    sum++;
-            }
-        }
-
-        return sum.ToString();
+        return count.ToString();
     }
 
     public override string SolvePartTwo(string input) => string.Empty;
-
-    private readonly record struct Key(int[] Values);
-    private readonly record struct Lock(int[] Values);
-
-    private static bool DoFit(Key k, Lock l) =>
-        k.Values.Select((v, i) => (v, i)).All(t => t.v + l.Values[t.i] <= 5);
 }
