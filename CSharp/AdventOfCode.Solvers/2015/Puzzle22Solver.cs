0ed377c2384cdb53c6d992a410094b565926f4d4
Andrew <5436387+fidwell@users.noreply.github.com>
2024-12-26 00:10:55 -0500

    2015 puzzle 22 - some boilerplate

---
+++ CSharp/AdventOfCode.Solvers/2015/Puzzle22Solver.cs
@@ -0,0 +1,28 @@
+﻿using AdventOfCode.Core.StringUtilities;
+
+namespace AdventOfCode.Solvers._2015;
+
+public class Puzzle22Solver : PuzzleSolver
+{
+    public override string SolvePartOne(string input)
+    {
+        var lines = input.SplitByNewline().ToArray();
+        var bossHp = int.Parse(Regexes.NonNegativeInteger().Match(lines[0]).Value);
+        var bossDamge = int.Parse(Regexes.NonNegativeInteger().Match(lines[1]).Value);
+        throw new NotImplementedException();
+    }
+
+    public override string SolvePartTwo(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public enum Spell
+    {
+        MagicMissile,
+        Drain,
+        Shield,
+        Poison,
+        Recharge
+    }
+}
