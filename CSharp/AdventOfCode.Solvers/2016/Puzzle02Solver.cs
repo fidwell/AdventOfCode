1f8a1137000958b7711d52d8d0417a9a9d167ba1
Andrew <5436387+fidwell@users.noreply.github.com>
2025-01-10 22:40:54 -0500

    2016 puzzle 2

---
+++ CSharp/AdventOfCode.Solvers/2016/Puzzle02Solver.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using AdventOfCode.Core.Geometry;
+using AdventOfCode.Core.StringUtilities;
+
+namespace AdventOfCode.Solvers._2016;
+
+public class Puzzle02Solver : PuzzleSolver
+{
+    public override string SolvePartOne(string input) => Solve(input, new CharacterMatrix("123\r\n456\r\n789"));
+    public override string SolvePartTwo(string input) => Solve(input, new CharacterMatrix("..1..\r\n.234.\r\n56789\r\n.ABC.\r\n..D.."));
+
+    private static string Solve(string input, CharacterMatrix keyPad)
+    {
+        var location = keyPad.SingleMatch('5');
+        var lines = input.SplitByNewline();
+        var code = new StringBuilder();
+
+        foreach (var line in lines)
+        {
+            foreach (var instruction in line)
+            {
+                var direction = instruction.ToDirection();
+                var newLocation = location.Go(direction);
+                if (keyPad.IsInBounds(newLocation) && keyPad.CharAt(newLocation) != '.')
+                {
+                    location = newLocation;
+                }
+            }
+            var keyValue = keyPad.CharAt(location);
+            code.Append(keyValue);
+        }
+
+        return code.ToString();
+    }
+}
--- CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2016.cs
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2016.cs
@@ -11,4 +11,12 @@ public class SolutionVerifier2016() : SolutionVerifier(2016)
     [DataRow(2, false, "152", DisplayName = "2016.01.2-problem")]
     public void Solve_2016_01(int part, bool useExample, string expected) =>
         Solve(new Puzzle01Solver(), 1, part, useExample, expected);
+
+    [DataTestMethod, Timeout(Timeout)]
+    [DataRow(1, true, "1985", DisplayName = "2016.01.1-example")]
+    [DataRow(1, false, "84452", DisplayName = "2016.01.1-problem")]
+    [DataRow(2, true, "5DB3", DisplayName = "2016.01.2-example")]
+    [DataRow(2, false, "D65C3", DisplayName = "2016.01.2-problem")]
+    public void Solve_2016_02(int part, bool useExample, string expected) =>
+        Solve(new Puzzle02Solver(), 2, part, useExample, expected);
 }
