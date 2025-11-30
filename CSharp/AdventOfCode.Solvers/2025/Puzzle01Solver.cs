0f2116df7c28e8b2ecf7ad192abff156852b4441
Andrew <5436387+fidwell@users.noreply.github.com>
2025-11-30 17:14:02 -0500

    Prep for 2025

---
+++ CSharp/AdventOfCode.Solvers/2025/Puzzle01Solver.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode.Solvers._2025;
+
+public class Puzzle01Solver : PuzzleSolver
+{
+    public override string SolvePartOne(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string SolvePartTwo(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2025.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode.Solvers._2025;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Tests.SolutionVerifiers;
+
+[TestClass]
+public class SolutionVerifier2025() : SolutionVerifier(2025)
+{
+    [DataTestMethod, Timeout(Timeout)]
+    [DataRow(1, true, "", DisplayName = "2025.01.1-example")]
+    [DataRow(1, false, "", DisplayName = "2025.01.1-problem")]
+    [DataRow(2, true, "", DisplayName = "2025.01.2-example")]
+    [DataRow(2, false, "", DisplayName = "2025.01.2-problem")]
+    public void Solve_2025_01(int part, bool useExample, string expected) =>
+        Solve(new Puzzle01Solver(), 1, part, useExample, expected);
+}
