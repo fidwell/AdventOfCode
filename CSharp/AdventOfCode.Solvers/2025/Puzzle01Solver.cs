190afe7d3a28481dfc311c71856a9354e6889f43
Andrew <5436387+fidwell@users.noreply.github.com>
2025-12-01 00:58:03 -0500

    2025 puzzle 1

--- CSharp/AdventOfCode.Solvers/2025/Puzzle01Solver.cs
+++ CSharp/AdventOfCode.Solvers/2025/Puzzle01Solver.cs
@@ -1,14 +1,52 @@
-﻿namespace AdventOfCode.Solvers._2025;
+﻿using AdventOfCode.Core.StringUtilities;
+
+namespace AdventOfCode.Solvers._2025;
 
 public class Puzzle01Solver : PuzzleSolver
 {
-    public override string SolvePartOne(string input)
-    {
-        throw new NotImplementedException();
-    }
+    public override string SolvePartOne(string input) => Solve(input, false).ToString();
+    public override string SolvePartTwo(string input) => Solve(input, true).ToString();
 
-    public override string SolvePartTwo(string input)
+    public static int Solve(string input, bool countPasses)
     {
-        throw new NotImplementedException();
+        var directions = input.SplitByNewline();
+        var zeros = 0;
+        var dial = 50;
+        foreach (var direction in directions)
+        {
+            var orientation = direction[0];
+            var isGoingUp = orientation == 'R';
+            var amount = int.Parse(direction[1..]) *
+                (isGoingUp ? 1 : -1);
+
+            var passes = Math.Abs(amount) / 100;
+            var effectiveAmount = amount % 100;
+
+            if (!isGoingUp && dial == 0)
+                dial = 100;
+
+            dial += effectiveAmount;
+
+            if (isGoingUp && dial == 100)
+                dial = 0;
+
+            while (dial > 99)
+            {
+                dial -= 100;
+                passes++;
+            }
+            while (dial < 0)
+            {
+                dial += 100;
+                passes++;
+            }
+
+            if (dial == 0)
+                zeros++;
+
+            if (countPasses)
+                zeros += passes;
+        }
+        return zeros;
     }
 }
--- CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2025.cs
+++ CSharp/AdventOfCode.Tests/SolutionVerifiers/SolutionVerifier2025.cs
@@ -7,10 +7,10 @@ namespace AdventOfCode.Tests.SolutionVerifiers;
 public class SolutionVerifier2025() : SolutionVerifier(2025)
 {
     [DataTestMethod, Timeout(Timeout)]
-    [DataRow(1, true, "", DisplayName = "2025.01.1-example")]
-    [DataRow(1, false, "", DisplayName = "2025.01.1-problem")]
-    [DataRow(2, true, "", DisplayName = "2025.01.2-example")]
-    [DataRow(2, false, "", DisplayName = "2025.01.2-problem")]
+    [DataRow(1, true, "3", DisplayName = "2025.01.1-example")]
+    [DataRow(1, false, "1132", DisplayName = "2025.01.1-problem")]
+    [DataRow(2, true, "6", DisplayName = "2025.01.2-example")]
+    [DataRow(2, false, "6623", DisplayName = "2025.01.2-problem")]
     public void Solve_2025_01(int part, bool useExample, string expected) =>
         Solve(new Puzzle01Solver(), 1, part, useExample, expected);
 }
