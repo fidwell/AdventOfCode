d7e9d39d6f6bd15b02ba56ccde21eb4e9b52b86c
Andrew <5436387+fidwell@users.noreply.github.com>
2025-12-09 11:08:00 -0500

    22025 puzzle 9 part 2 fast and clean(ish)

--- CSharp/AdventOfCode.Solvers/2025/Puzzle09Solver.cs
+++ CSharp/AdventOfCode.Solvers/2025/Puzzle09Solver.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.RegularExpressions;
-using AdventOfCode.Core.StringUtilities;
+﻿using AdventOfCode.Core.StringUtilities;
 
 namespace AdventOfCode.Solvers._2025;
 
@@ -7,17 +6,12 @@ public partial class Puzzle09Solver : PuzzleSolver
 {
     public override object SolvePartOne(string input)
     {
-        var redTiles = input.SplitByNewline().Select(l =>
-        {
-            var match = CoordinateRegex().Match(l);
-            return new Coord(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
-        }).ToList();
-
+        var redTiles = ParseData(input);
         var largestSize = 0L;
 
-        for (var i = 0; i < redTiles.Count - 1; i++)
+        for (var i = 0; i < redTiles.Length - 1; i++)
         {
-            for (var j = i + 1; j < redTiles.Count; j++)
+            for (var j = i + 1; j < redTiles.Length; j++)
             {
                 var width = Math.Abs(redTiles[i].Item1 - redTiles[j].Item1) + 1;
                 var height = Math.Abs(redTiles[i].Item2 - redTiles[j].Item2) + 1;
@@ -32,14 +26,14 @@ public partial class Puzzle09Solver : PuzzleSolver
     public override object SolvePartTwo(string input)
     {
         // Step 1: Parse data
-        var cornerCoordinates = input.SplitByNewline().Select((l, i) =>
-        {
-            var match = CoordinateRegex().Match(l);
-            return new Coord(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
-        }).ToArray();
+        var cornerCoordinates = ParseData(input);
 
         // Step 2: Find edges
-        var edges = new List<List<Coord>>();
+        var expandedEdges = new List<List<Coord>>();
+        var edgeDefinitions = new List<(Coord, Coord)>();
+
+        var allTiles = new HashSet<Coord>(cornerCoordinates);
+
         for (var i = 0; i < cornerCoordinates.Length; i++)
         {
             var nextI = i == cornerCoordinates.Length - 1 ? 0 : i + 1;
@@ -47,32 +41,29 @@ public partial class Puzzle09Solver : PuzzleSolver
             var here = cornerCoordinates[i];
             var next = cornerCoordinates[nextI];
 
-            var edge = new List<Coord>();
+            // Precompute "ideal" edge (lower coordinates first)
+            var minX = Math.Min(here.Item1, next.Item1);
+            var maxX = Math.Max(here.Item1, next.Item1);
+            var minY = Math.Min(here.Item2, next.Item2);
+            var maxY = Math.Max(here.Item2, next.Item2);
+            edgeDefinitions.Add(((minX, minY), (maxX, maxY)));
 
-            if (here.Item1 == next.Item1)
+            // Add to list of all tiles in set
+            if (minX == maxX)
             {
-                var hereY = Math.Min(here.Item2, next.Item2);
-                var nextY = Math.Max(here.Item2, next.Item2);
-                // Same x; going up or down
-                for (var y = hereY; y <= nextY; y++)
+                for (var y = minY + 1; y < maxY; y++)
                 {
-                    edge.Add((here.Item1, y));
+                    allTiles.Add((minX, y));
                 }
             }
             else
             {
-                var hereX = Math.Min(here.Item1, next.Item1);
-                var nextX = Math.Max(here.Item1, next.Item1);
-                // Same y; going left or right
-                for (var x = hereX; x <= nextX; x++)
+                for (var x = minX + 1; x < maxX; x++)
                 {
-                    edge.Add((x, here.Item2));
+                    allTiles.Add((x, minY));
                 }
             }
-            edges.Add(edge);
         }
-        var edgeCoordinates = edges.SelectMany(c => c).ToHashSet();
-        var allTiles = new HashSet<Coord>([.. cornerCoordinates, .. edgeCoordinates]);
 
         // Step 3: Iterate over all rectangles
         var biggestX = cornerCoordinates.Max(t => t.Item1);
@@ -88,16 +79,12 @@ public partial class Puzzle09Solver : PuzzleSolver
                 var minY = Math.Min(item1.Item2, item2.Item2);
                 var maxY = Math.Max(item1.Item2, item2.Item2);
 
-                // Skip skinny rectangles
-                if (minX >= maxX - 1 || minY >= maxY - 1)
-                    continue;
-
                 var rectArea = (long)(maxX - minX + 1) * (maxY - minY + 1);
-                if (rectArea < largestArea || rectArea < 169172640)
+                if (rectArea < largestArea)
                     continue;
 
                 var center = ((minX + maxX) / 2, (maxY + minY) / 2);
-                var anyIntersect = edges.Any(l => DoesLineIntersectWithRectangle(item1, item2, l));
+                var anyIntersect = edgeDefinitions.Any(e => DoesEdgeIntersectWithRectangle(item1, item2, e));
                 if (anyIntersect)
                     continue;
 
@@ -105,20 +92,29 @@ public partial class Puzzle09Solver : PuzzleSolver
                 if (centerIsInsidePolygon)
                 {
                     largestArea = rectArea;
-                    Console.WriteLine($"{item1},{item2}: {largestArea}");
                 }
             }
         }
         return largestArea;
     }
 
-    private static bool DoesLineIntersectWithRectangle(Coord item1, Coord item2, List<Coord> line)
+    private static Coord[] ParseData(string input) =>
+        [.. input.SplitByNewline().Select((l, i) =>
+        {
+            var values = l.Split(',').Select(int.Parse).ToArray();
+            return new Coord(values[0], values[1]);
+        })];
+
+    private static bool DoesEdgeIntersectWithRectangle(Coord item1, Coord item2, (Coord, Coord) edge)
     {
         var minX = Math.Min(item1.Item1, item2.Item1);
         var maxX = Math.Max(item1.Item1, item2.Item1);
         var minY = Math.Min(item1.Item2, item2.Item2);
         var maxY = Math.Max(item1.Item2, item2.Item2);
-        return line.Any(l => l.Item1 > minX && l.Item1 < maxX && l.Item2 > minY && l.Item2 < maxY);
+
+        return edge.Item1.Item2 == edge.Item2.Item2
+            ? edge.Item1.Item2 > minY && edge.Item1.Item2 < maxY && edge.Item2.Item1 > minX && edge.Item1.Item1 < maxX
+            : edge.Item1.Item1 > minX && edge.Item1.Item1 < maxX && edge.Item2.Item2 > minY && edge.Item1.Item2 < maxY;
     }
 
     private static bool IsInsidePolygon(HashSet<Coord> boundaryPoints, Coord pointToTest, int maxX)
@@ -156,7 +152,4 @@ public partial class Puzzle09Solver : PuzzleSolver
 
         return boundariesHit % 2 == 1;
     }
-
-    [GeneratedRegex(@"(\d+),(\d+)")]
-    private static partial Regex CoordinateRegex();
 }
