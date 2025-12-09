6ccaabe8f9131dc172e6b9b8c1ddc1601dd660ee
Andrew <5436387+fidwell@users.noreply.github.com>
2025-12-09 02:51:08 -0500

    2025 puzzle 9 part 2 wip

--- CSharp/AdventOfCode.Solvers/2025/Puzzle09Solver.cs
+++ CSharp/AdventOfCode.Solvers/2025/Puzzle09Solver.cs
@@ -30,32 +30,31 @@ public class Puzzle09Solver : PuzzleSolver
         return largestSize;
     }
 
+    private record struct Tile(int Id, Coord Coord);
+
+    //public object SolvePartTwoAlternate(string input)
     public override object SolvePartTwo(string input)
     {
         // Step 1: Parse data
 
         var regex = new Regex(@"(\d+),(\d+)");
-        var redTiles = input.SplitByNewline().Select(l =>
+        var redTiles = input.SplitByNewline().Select((l, i) =>
         {
             var match = regex.Match(l);
             return new Coord(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
-        }).ToList();
+        }).ToArray();
 
-        // Step 2: Find loop
-
-        var maxX = redTiles.Max(t => t.Item1);
-        var maxY = redTiles.Max(t => t.Item2);
-
-        var coloredTiles = new HashSet<Coord>();
-
-        for (var i = 0; i < redTiles.Count; i++)
+        // Find lines
+        var lines = new List<List<Coord>>();
+        for (var i = 0; i < redTiles.Length; i++)
         {
-            var here = redTiles[i];
-            coloredTiles.Add(here);
+            var nextI = i == redTiles.Length - 1 ? 0 : i + 1;
 
-            var next = i == redTiles.Count - 1
-                ? redTiles[0]
-                : redTiles[i + 1];
+            var here = redTiles[i];
+            var next = redTiles[nextI];
+
+            var line = new List<Coord>();
+
             if (here.Item1 == next.Item1)
             {
                 var hereY = Math.Min(here.Item2, next.Item2);
@@ -63,7 +62,7 @@ public class Puzzle09Solver : PuzzleSolver
                 // Same x; going up or down
                 for (var y = hereY; y <= nextY; y++)
                 {
-                    coloredTiles.Add((here.Item1, y));
+                    line.Add((here.Item1, y));
                 }
             }
             else
@@ -73,39 +72,162 @@ public class Puzzle09Solver : PuzzleSolver
                 // Same y; going left or right
                 for (var x = hereX; x <= nextX; x++)
                 {
-                    coloredTiles.Add((x, here.Item2));
+                    line.Add((x, here.Item2));
                 }
             }
+            lines.Add(line);
         }
 
+        // Iterate over all rectangles
         var largestSize = 0L;
-
-        for (var i = 0; i < redTiles.Count - 1; i++)
+        for (var i = 0; i < redTiles.Length - 1; i++)
         {
-            for (var j = i + 1; j < redTiles.Count; j++)
+            for (var j = i + 1; j < redTiles.Length; j++)
             {
                 var size = Math.Abs((redTiles[i].Item1 - redTiles[j].Item1 + 1) *
                     (redTiles[i].Item2 - redTiles[j].Item2 + 1));
+                //Console.Write($"Checking {redTiles[i]} to {redTiles[j]} (size {size})...");
 
                 if (size < largestSize)
+                {
+                    //Console.WriteLine("too small");
                     continue;
+                }
 
-                Console.Write($"Checking {redTiles[i]} to {redTiles[j]} (size {size})...");
-                var isValid = AreAllCoordinatesInsidePolygon(coloredTiles, redTiles[i], redTiles[j], maxX + 1);
-                Console.WriteLine(isValid);
-
-                if (isValid && size > largestSize)
+                var isValid = !lines.Any(l => Intersect(redTiles[i], redTiles[j], l));
+                if (isValid)
                 {
                     Console.WriteLine(size);
                     largestSize = size;
                 }
             }
         }
-        return largestSize;
 
-        /*
-        // Print floor
-        if (maxY < 100)
+        return largestSize;
+    }
+
+    public static bool Intersect(Coord item1, Coord item2, List<Coord> line)
+    {
+        var minX = Math.Min(item1.Item1, item2.Item1);
+        var maxX = Math.Max(item1.Item1, item2.Item1);
+        var minY = Math.Min(item1.Item2, item2.Item2);
+        var maxY = Math.Max(item1.Item2, item2.Item2);
+        return line.Any(l => l.Item1 > minX && l.Item1 < maxX && l.Item2 > minY && l.Item2 < maxY);
+    }
+
+    //public override object SolvePartTwo(string input)
+    public object SolvePartTwoAlternate(string input)
+    {
+        // Step 1: Parse data
+
+        var regex = new Regex(@"(\d+),(\d+)");
+        var redTiles = input.SplitByNewline().Select((l, i) =>
+        {
+            var match = regex.Match(l);
+            return new Tile(i, new Coord(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value)));
+        }).ToArray();
+
+        // Step 1.5: Squish data
+        var originalData = redTiles.Select(t => t.Coord).ToArray();
+
+        Console.WriteLine("Squishing xs...");
+        for (var x = 0; x < 99999; x++)
+        {
+            var furtherTiles = redTiles.Where(t => t.Coord.Item1 > x);
+            if (!furtherTiles.Any())
+                break;
+
+            var unchangedTiles = redTiles.Where(t => t.Coord.Item1 <= x);
+            var minX = furtherTiles.Min(t => t.Coord.Item1);
+            var diff = minX - x;
+            redTiles = [.. unchangedTiles, .. furtherTiles.Select(t => new Tile(t.Id, new Coord(t.Coord.Item1 - diff, t.Coord.Item2)))];
+        }
+        Console.WriteLine("Squishing ys...");
+        for (var y = 0; y < 99999; y++)
+        {
+            var furtherTiles = redTiles.Where(t => t.Coord.Item2 > y);
+            if (!furtherTiles.Any())
+                break;
+
+            var unchangedTiles = redTiles.Where(t => t.Coord.Item2 <= y);
+            var minY = furtherTiles.Min(t => t.Coord.Item2);
+            var diff = minY - y;
+            redTiles = [.. unchangedTiles, .. furtherTiles.Select(t => new Tile(t.Id, new Coord(t.Coord.Item1, t.Coord.Item2 - diff)))];
+        }
+
+        redTiles = [.. redTiles.OrderBy(t => t.Id)];
+
+        // Step 2: Find loop
+
+        var maxX = redTiles.Max(t => t.Coord.Item1);
+        var maxY = redTiles.Max(t => t.Coord.Item2);
+
+        var coloredTiles = new HashSet<Coord>();
+
+        for (var i = 0; i < redTiles.Length; i++)
+        {
+            var here = redTiles[i];
+            coloredTiles.Add(here.Coord);
+
+            var next = i == redTiles.Length - 1
+                ? redTiles[0]
+                : redTiles[i + 1];
+            if (here.Coord.Item1 == next.Coord.Item1)
+            {
+                var hereY = Math.Min(here.Coord.Item2, next.Coord.Item2);
+                var nextY = Math.Max(here.Coord.Item2, next.Coord.Item2);
+                // Same x; going up or down
+                for (var y = hereY; y <= nextY; y++)
+                {
+                    coloredTiles.Add((here.Coord.Item1, y));
+                }
+            }
+            else
+            {
+                var hereX = Math.Min(here.Coord.Item1, next.Coord.Item1);
+                var nextX = Math.Max(here.Coord.Item1, next.Coord.Item1);
+                // Same y; going left or right
+                for (var x = hereX; x <= nextX; x++)
+                {
+                    coloredTiles.Add((x, here.Coord.Item2));
+                }
+            }
+        }
+
+        // Step 3: Find largest valid rectangle
+
+        var largestSize = 0L;
+
+        for (var i = 0; i < redTiles.Length - 1; i++)
+        {
+            for (var j = i + 1; j < redTiles.Length; j++)
+            {
+                var size = Math.Abs((originalData[i].Item1 - originalData[j].Item1 + 1) *
+                    (originalData[i].Item2 - originalData[j].Item2 + 1));
+
+                //Console.Write($"Checking {originalData[i]} to {originalData[j]} (size {size})...");
+
+                if (size < largestSize)
+                {
+                    //Console.WriteLine("too small");
+                    continue;
+                }
+
+                var isValid = AreAllCoordinatesInsidePolygon(coloredTiles, redTiles[i].Coord, redTiles[j].Coord, maxX + 1);
+                //Console.WriteLine(isValid);
+
+                if (isValid)
+                {
+                    Console.WriteLine(size);
+                    largestSize = size;
+                }
+            }
+            var percentDone = i / (redTiles.Length - 1.0);
+            Console.WriteLine($"{percentDone:P} done");
+        }
+
+        // Print design
+        if (ShouldPrint)
         {
             for (var y = 0; y <= maxY; y++)
             {
@@ -117,15 +239,17 @@ public class Puzzle09Solver : PuzzleSolver
             }
         }
 
-        return 0;
-        */
+        // 2147475684 is TOO HIGH!
+        // 1516863759 is wrong
+        //  169172640 is TOO LOW
+        return largestSize;
     }
 
     private bool AreAllCoordinatesInsidePolygon(HashSet<Coord> vertices, Coord item1, Coord item2, int maxX)
     {
-        //return CoordinatesOfRectangleDefinedBy(item1, item2).All(c => IsInsidePolygon(vertices, c, maxX));
-        var v = VerticesOfRectangleDefinedBy(item1, item2);
-        return v.All(c => IsInsidePolygon(vertices, c, maxX));
+        return CoordinatesOfRectangleDefinedBy(item1, item2).All(c => IsInsidePolygon(vertices, c, maxX));
+        //var v = VerticesOfRectangleDefinedBy(item1, item2);
+        //return v.All(c => IsInsidePolygon(vertices, c, maxX));
     }
 
     private static HashSet<Coord> VerticesOfRectangleDefinedBy(Coord item1, Coord item2) =>
@@ -167,8 +291,19 @@ public class Puzzle09Solver : PuzzleSolver
         var boundariesHit = 0;
         for (var xr = pointToTest.Item1 + 1; xr <= maxX + 1; xr++)
         {
-            if (vertices.Contains((xr, pointToTest.Item2)))
+            if (vertices.Contains((xr, pointToTest.Item2)) &&
+                vertices.Contains((xr - 1, pointToTest.Item2)))
+            {
+                // Coincides with an edge
+                boundariesHit--;
+                while (vertices.Contains((xr, pointToTest.Item2)))
+                    xr++;
+            }
+            else if (vertices.Contains((xr, pointToTest.Item2)))
+            {
+                // Crossing an edge
                 boundariesHit++;
+            }
         }
 
         var result = boundariesHit % 2 == 1;
