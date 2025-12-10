f918b1d49cfb75c81857a17b46c4a5f2b68e88e0
Andrew <5436387+fidwell@users.noreply.github.com>
2025-12-10 15:32:50 -0500

    2025 puzzle 10 part 2 - Solved with an ILP library

---
+++ CSharp/AdventOfCode.Core/Matrixes/MatrixExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Google.OrTools.LinearSolver;
+
+namespace AdventOfCode.Core.Matrixes;
+
+public class MatrixExtensions
+{
+    public static int[] SolveSystemOfLinearEquations(int[,] coefficients, int[] solutions)
+    {
+        var equationCount = coefficients.GetLength(0);
+        var variableCount = coefficients.GetLength(1);
+
+        double[] solutionsAsDouble = [.. solutions.Select(s => (double)s)];
+
+        var solver = Solver.CreateSolver("SCIP");
+
+        var solverVariables = new Variable[variableCount];
+        for (var i = 0; i < variableCount; i++)
+        {
+            solverVariables[i] = solver.MakeIntVar(0, int.MaxValue, $"p_{i}");
+        }
+
+        for (var solution = 0; solution < equationCount; solution++)
+        {
+            var constraint = solver.MakeConstraint(solutions[solution], solutions[solution], $"J_{solution}");
+            for (var variable = 0; variable < variableCount; variable++)
+            {
+                constraint.SetCoefficient(solverVariables[variable], coefficients[solution, variable]);
+            }
+        }
+
+        var objective = solver.Objective();
+        for (int i = 0; i < variableCount; i++)
+        {
+            objective.SetCoefficient(solverVariables[i], 1);
+        }
+        objective.SetMinimization();
+
+        var resultStatus = solver.Solve();
+        if (resultStatus == Solver.ResultStatus.OPTIMAL)
+        {
+            var objectiveValue = solver.Objective().Value();
+            return [.. solverVariables.Select(v => (int)v.SolutionValue())];
+        }
+        else
+        {
+            // No solution found
+            return [];
+        }
+    }
+}
--- CSharp/AdventOfCode.Solvers/2025/Puzzle10Solver.cs
+++ CSharp/AdventOfCode.Solvers/2025/Puzzle10Solver.cs
@@ -1,70 +1,58 @@
-﻿using AdventOfCode.Core.StringUtilities;
-using AdventOfCode.Solvers.Common;
+﻿using AdventOfCode.Core.Matrixes;
+using AdventOfCode.Core.StringUtilities;
 
 namespace AdventOfCode.Solvers._2025;
 
 public class Puzzle10Solver : PuzzleSolver
 {
     public override object SolvePartOne(string input) =>
-        input.SplitByNewline().Select(l => new Machine(l)).Sum(m => m.FewestPressesToActivate());
+        input.SplitByNewline().Select(l => new Machine(l))
+        .Sum(m => m.FewestPressesToActivate());
 
-    public override object SolvePartTwo(string input)
-    {
-        var machines = input.SplitByNewline().Select(l => new Machine(l));
-
-        var sum = 0;
-        object lockObj = new();
-        Parallel.ForEach(machines, machine =>
-        {
-            lock (lockObj)
-            {
-                sum += machine.FewestPressesToConfigureJoltage();
-            }
-        });
-
-        return sum;
-    }
+    public override object SolvePartTwo(string input) =>
+        input.SplitByNewline().Select(l => new Machine(l))
+        .Sum(m => m.FewestPressesToConfigureJoltage());
 
     private class Machine
     {
-        public bool[] TargetIndicatorLights;
         public int[][] ButtonWiringSchematics;
         public int[] JoltageRequirements;
 
-        public int TargetIndicatorLightsValue;
-        public int[] ButtonWiringSchematicsValues;
-        public int[] CurrentJoltages;
+        // On/off values of each light represented as a bit mask.
+        // e.g. 5 => 0101 => light 0 on, light 1 off, etc.
+        public int TargetIndicatorLights;
 
         public Machine(string input)
         {
             var chunks = input.Split(' ').ToList();
-            TargetIndicatorLights = [.. chunks[0][1..^1].Select(c => c == '#')];
             ButtonWiringSchematics = [.. chunks[1..^1].Select(ch => ch[1..^1].Split(',').Select(int.Parse).ToArray())];
             JoltageRequirements = [.. chunks[^1][1..^1].Split(',').Select(int.Parse)];
-            CurrentJoltages = new int[JoltageRequirements.Length];
 
-            TargetIndicatorLightsValue = TargetIndicatorLights.Select((b, i) => (b, i)).Sum(p => p.b ? (int)Math.Pow(2, p.i) : 0);
-            ButtonWiringSchematicsValues = [.. ButtonWiringSchematics.Select(bws => (int)bws.Sum(b => Math.Pow(2, b)))];
+            TargetIndicatorLights = chunks[0][1..^1].Select(c => c == '#')
+                .Select((b, i) => (b, i)).Sum(p => p.b ? (int)Math.Pow(2, p.i) : 0);
         }
 
         public int FewestPressesToActivate()
         {
-            var numOptions = Math.Pow(2, ButtonWiringSchematicsValues.Length);
+            // 5 => 0101 => light 0 is toggled, light 1 is not toggled, etc.
+            var buttonWiringSchematicsValues = ButtonWiringSchematics.Select(bws => (int)bws.Sum(b => Math.Pow(2, b))).ToArray();
+
+            var numOptions = Math.Pow(2, buttonWiringSchematicsValues.Length);
             var min = 65535;
             for (var x = 1; x < numOptions; x++)
             {
                 var total = 0;
                 var bUsed = 0;
-                for (var b = 0; b < ButtonWiringSchematicsValues.Length; b++)
+                for (var b = 0; b < buttonWiringSchematicsValues.Length; b++)
                 {
                     var useButton = (x >> b) % 2 == 1;
                     if (useButton)
                     {
-                        total ^= ButtonWiringSchematicsValues[b];
+                        total ^= buttonWiringSchematicsValues[b];
                         bUsed++;
                     }
                 }
-                if (total == TargetIndicatorLightsValue && bUsed < min)
+                if (total == TargetIndicatorLights && bUsed < min)
                     min = bUsed;
             }
             return min;
@@ -72,55 +60,41 @@ public class Puzzle10Solver : PuzzleSolver
 
         public int FewestPressesToConfigureJoltage()
         {
-            var queue = new PriorityQueue<JoltageState, int>();
-            queue.Enqueue(new JoltageState(new int[JoltageRequirements.Length], 0), 0);
+            // System of linear equations. For buttons b0 through bn, pressing each p0 through
+            // pn times, find the values for joltages J0 through Jm.
 
-            while (queue.Count > 0)
+            // For each joltage Jm, bit mask each button schematic bi = bi^m to find
+            // whether that button modifies that joltage.
+            // Sum of the number of presses of each button should then equal Jm.
+            // J0 = p0*b0^0 + p1*b1^0 + ... + pn*bn^0
+            // J1 = p0*b0^1 + p1*b1^1 + ... + pn*bn^1
+
+            // For example machine 1 this becomes:
+            // 3 = p0*b0^0 + p1*b1^0 + p2*b2^0 + p4*b4^0 + p5*b5^0 + p6*b6^0
+            // 5 = p0*b0^1 + p1*b1^1 + p2*b2^1 + p4*b4^1 + p5*b5^1 + p6*b6^1
+            // 4 = p0*b0^2 + p1*b1^2 + p2*b2^2 + p4*b4^2 + p5*b5^2 + p6*b6^2
+            // 7 = p0*b0^3 + p1*b1^3 + p2*b2^3 + p4*b4^3 + p5*b5^3 + p6*b6^3
+
+            // 3 = p0*0 + p1*0 + p2*0 + p4*0 + p5*1 + p6*1 (110000 or 48 --- only buttons 5 and 6 operate on J0)
+            // 5 = p0*0 + p1*1 + p2*0 + p4*0 + p5*0 + p6*1 (100010 or 34)
+            // 4 = p0*0 + p1*0 + p2*1 + p4*1 + p5*1 + p6*0 (011100 or 28)
+            // 7 = p0*1 + p1*1 + p2*0 + p4*1 + p5*0 + p6*0 (001011 or 11)
+
+            // p0 through p6 are the unknowns
+            // Solution array J0..Jm is simply JoltageRequirements
+            // Coefficient matrix is the bit representation of buttonOperationMasks
+            var coefficientMatrix = new int[JoltageRequirements.Length, ButtonWiringSchematics.Length];
+
+            for (var j = 0; j < JoltageRequirements.Length; j++)
             {
-                var thisState = queue.Dequeue();
-                if (Enumerable.SequenceEqual(thisState.Joltages, JoltageRequirements))
+                for (var b = 0; b < ButtonWiringSchematics.Length; b++)
                 {
-                    Console.WriteLine($"Found solution for a single machine in {thisState.TotalPresses} presses");
-                    return thisState.TotalPresses;
-                }
-
-                // Generate next states
-                foreach (var schematic in ButtonWiringSchematics)
-                {
-                    var beforePressing = thisState.Joltages.ToArray();
-                    var isValidState = true;
-                    foreach (var button in schematic)
-                    {
-                        beforePressing[button] += 1;
-
-                        if (beforePressing[button] > JoltageRequirements[button])
-                        {
-                            isValidState = false;
-                            break;
-                        }
-                    }
-
-                    if (isValidState)
-                    {
-                        var nextState = new JoltageState(beforePressing, thisState.TotalPresses + 1);
-                        queue.Enqueue(nextState, DistanceToTarget(beforePressing));
-                    }
+                    if (ButtonWiringSchematics[b].Contains(j))
+                        coefficientMatrix[j, b] = 1;
                 }
             }
 
-            throw new SolutionNotFoundException();
+            return MatrixExtensions.SolveSystemOfLinearEquations(coefficientMatrix, JoltageRequirements).Sum();
         }
-
-        private int DistanceToTarget(int[] state)
-        {
-            var result = 0;
-            for (var i = 0; i < state.Length; i++)
-            {
-                result += JoltageRequirements[i] - state[i];
-            }
-            return result;
-        }
-
-        private record struct JoltageState(int[] Joltages, int TotalPresses);
     }
 }
